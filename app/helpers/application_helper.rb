6bf02b136ef4822a45b188722739426db294474e
Matt Critchlow <mcritchlow@ucsd.edu>
2013-01-22 15:25:48 -0800

    update full_title to check with blank method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def full_title(page_title)
     base_title = "Digital Library Collections"
-    if(page_title.present?)
+    if(page_title.blank?)
       base_title
     else
       "#{base_title} | #{page_title}"
