6991ba746770fd1930d3637ecb0d348e49a166ba
Esme Cowles <escowles@ucsd.edu>
2013-01-22 14:15:35 -0800

    Adding omniauth

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'hydra-head'
 gem 'sqlite3'
 
 gem 'devise'
+gem 'omniauth'
+gem 'omniauth-shibboleth'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121210190838) do
+ActiveRecord::Schema.define(:version => 20130122220658) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(:version => 20121210190838) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "guest",                  :default => false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
