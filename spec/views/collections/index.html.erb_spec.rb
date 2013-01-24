a102070b3233ab36ed92a50e572bcc7485e780d4
Matt Critchlow <mcritchlow@ucsd.edu>
2013-01-24 10:13:33 -0800

    add plain collection index and title test

--- spec/views/collections/index.html.erb_spec.rb
+++ spec/views/collections/index.html.erb_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe "collections/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have an h1 title" do
+    render
+    expect(rendered).to match /Digital Library Collections/
+  end
 end
+
