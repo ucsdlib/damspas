6b61b3b96303e998402c144e4f234905c29e9c36
Huawei Weng <hweng@ucsd.edu>
2013-01-25 13:20:23 -0800

    add breadcrumb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,26 +13,26 @@ module ApplicationHelper
   # TODO: what is needed? action, controller?, page title?
   # params[:action],params[:controller],...
 
-    
-  def breadcrumbs(url)
-    s = ""
-  #  url = request.path.split('?')  #remove extra query string parameters
-    levels = url[0].split('/') #break up url into different levels
-    levels.each_with_index do |level, index|
-      unless level.blank?
-        if index == levels.size-1 || 
-           (level == levels[levels.size-2] && levels[levels.size-1].to_i > 0)
-          s += "<li>#{level.gsub(/_/, ' ')}</li>\n" unless level.to_i > 0
-        else
-            link = "/"
-            i = 1
-            while i <= index
-              link += "#{levels[i]}/"
-              i+=1
-            end
-            s += "<li><a href=\"#{link}\">#{level.gsub(/_/, ' ')}</a></li>\n"
-        end
-      end
-   end
- end
+  def breadcrumbs
+
+result = "".html_safe
+
+ 
+
+association_chain.each_with_index do |item, index|
+
+# note that .name works for both classes and objects
+
+result << link_to(item.name.humanize.titlecase, association_chain[0..index])
+
+result << " &raquo; ".html_safe
+
+end
+
+ 
+
+result << resource.name.humanize.titleize
+
+end
+ 
 end 
