87bcb6c046ef938aa17893bfbe4a94e0c6c9da1e
Huawei Weng <hweng@ucsd.edu>
2013-01-25 09:34:14 -0800

    add breadcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,27 @@ module ApplicationHelper
   # placeholder for rendering breadcrumbs
   # TODO: what is needed? action, controller?, page title?
   # params[:action],params[:controller],...
-  def breadcrumbs()
-    base_breadcrumb = "Home"
-  end
-end
+
+    
+  def breadcrumbs(url)
+    s = ""
+  #  url = request.path.split('?')  #remove extra query string parameters
+    levels = url[0].split('/') #break up url into different levels
+    levels.each_with_index do |level, index|
+      unless level.blank?
+        if index == levels.size-1 || 
+           (level == levels[levels.size-2] && levels[levels.size-1].to_i > 0)
+          s += "<li>#{level.gsub(/_/, ' ')}</li>\n" unless level.to_i > 0
+        else
+            link = "/"
+            i = 1
+            while i <= index
+              link += "#{levels[i]}/"
+              i+=1
+            end
+            s += "<li><a href=\"#{link}\">#{level.gsub(/_/, ' ')}</a></li>\n"
+        end
+      end
+   end
+ end
+end 
