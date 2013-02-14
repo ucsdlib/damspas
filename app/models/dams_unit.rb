8bd22d4fdfc43108dda9d85a250605b4c05272a5
Matt Critchlow <mcritchlow@ucsd.edu>
2013-02-14 10:09:51 -0800

    add unit edit view error handling

--- app/models/dams_unit.rb
+++ app/models/dams_unit.rb
@@ -1,4 +1,14 @@
 class DamsUnit < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => DamsUnitDatastream
   delegate_to "damsMetadata", [:name,:description,:uri,:code]
+  
+  #lowercase the code
+  before_save { self.code.downcase! }
+  
+  # all unit properties are currently required
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :uri, presence: true
+  validates :code, presence: true
+  
 end
