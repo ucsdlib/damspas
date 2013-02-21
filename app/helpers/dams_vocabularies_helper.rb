41b7ff694e5054215beab4372dd606d8dd84f75b
Esmé Cowles <escowles@ucsd.edu>
2013-02-21 10:17:29 -0500

    Moving dams_vocabularies views

--- app/helpers/dams_vocabularies_helper.rb
+++ app/helpers/dams_vocabularies_helper.rb
@@ -1,7 +1,6 @@
 module DamsVocabulariesHelper
-  def render_vocabularies_list 
-    #stub vocabularies list
-    render :partial => "dams_vocabulariess/vocabularies_links", :collection => DamsVocabulary.all, :as => :dams_vocabulary
+  def render_vocabulary_list 
+    render :partial => "dams_vocabularies/vocabulary_links", :collection => DamsVocabulary.all, :as => :dams_vocabulary
   end
 
   def current_vocabulary
