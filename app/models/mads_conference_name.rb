57c83bcb1441e283fcafa4e5c44ab1d7fae3e168
VivianChu <tchu@ucsd.edu>
2013-02-26 10:07:00 -0800

    Updating MadsConference Model

--- app/models/mads_conference_name.rb
+++ app/models/mads_conference_name.rb
@@ -1,5 +1,5 @@
 class MadsConferenceName < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => MadsConferenceNameDatastream 
-  delegate_to "damsMetadata", [:name, :sameAs, :elementList, :authority]
+  delegate_to "damsMetadata", [:name, :sameAs, :elementList, :authority, :type]
   
 end
