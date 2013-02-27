eb4c21fe597db7b0c09b0c0884c7f578e11b1b7e
Esmé Cowles <escowles@ucsd.edu>
2013-02-27 15:49:01 -0500

    Adding new Name/Note/Subject predicates

--- app/models/vocabulary/mads_vocabulary.rb
+++ app/models/vocabulary/mads_vocabulary.rb
@@ -1,17 +1,28 @@
 class MADS < RDF::Vocabulary("http://www.loc.gov/mads/rdf/v1#")
   property :complexSubject
+  property :ComplexSubject
   property :authoritativeLabel
   property :elementList
   property :elementValue
+  property :Topic
   property :TopicElement
+  property :Temporal
   property :TemporalElement
   property :FullNameElement
   property :DateNameElement
   property :FamilyNameElement
   property :GivenNameElement
+  property :Name
   property :NameElement
   property :TermsOfAddressNameElement
+  property :GenreForm
   property :GenreFormElement
+  property :Geographic
   property :GeographicElement
+  property :Occupation
   property :OccupationElement
+  property :ConferenceName
+  property :CorporateName
+  property :FamilyName
+  property :PersonalName
 end
