1bea3ea5deb7968d5f98c7dd1562e98d95e9829b
VivianChu <tchu@ucsd.edu>
2013-02-27 10:51:58 -0800

    Adding Note and ScopeContentNote classes

---
+++ app/models/datastreams/dams_scope_content_note_datastream.rb
@@ -0,0 +1,13 @@
+class DamsScopeContentNoteDatastream < DamsDatastream
+  map_predicates do |map|    
+    map.value(:in=> RDF)
+    map.displayLabel(:in=>DAMS)
+    map.type(:in=>DAMS)
+  end
+
+  rdf_subject { |ds| RDF::URI.new(Rails.configuration.id_namespace + ds.pid)}
+  def serialize
+    graph.insert([rdf_subject, RDF.type, DAMS.ScopeContentNote]) if new?
+    super
+  end
+end
