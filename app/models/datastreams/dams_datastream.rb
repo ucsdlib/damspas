be666dc1e398aba0752d4cee21cbe9f715b63e75
VivianChu <tchu@ucsd.edu>
2013-02-28 15:40:36 -0800

    Adding DamsNote and DamsScopeContentNote to AssembledCollection

--- app/models/datastreams/dams_datastream.rb
+++ app/models/datastreams/dams_datastream.rb
@@ -8,14 +8,28 @@ class DamsDatastream < ActiveFedora::RdfxmlRDFDatastream
   
   def to_solr (solr_doc = {})
 	n = 0
-    Solrizer.insert_field(solr_doc, "note_#{n}_type", type)
-    Solrizer.insert_field(solr_doc, "note_#{n}_display_label", displayLabel)
-    Solrizer.insert_field(solr_doc, "note_#{n}_value", value)
+  #  Solrizer.insert_field(solr_doc, "note_#{n}_type", type)
+  #  Solrizer.insert_field(solr_doc, "note_#{n}_display_label", displayLabel)
+  #  Solrizer.insert_field(solr_doc, "note_#{n}_value", value)
 
-	n = 0
-    Solrizer.insert_field(solr_doc, "scope_content_note_#{n}_type", type)
-    Solrizer.insert_field(solr_doc, "scope_content_note_#{n}_display_label", displayLabel)
-    Solrizer.insert_field(solr_doc, "scope_content_note_#{n}_value", value)
+ #	n = 0
+ #   Solrizer.insert_field(solr_doc, "scope_content_note_#{n}_type", type)
+ #   Solrizer.insert_field(solr_doc, "scope_content_note_#{n}_display_label", displayLabel)
+ #   Solrizer.insert_field(solr_doc, "scope_content_note_#{n}_value", value)
+  
+  	list = elementList.first
+	i = 0
+	if list != nil
+		while i < list.size  do
+		  if (list[i].class == DamsDatastream::ListNote::ScopeContentNote)
+			Solrizer.insert_field(solr_doc, 'scope_content_note_#{i}_value', list[i].value.first)
+	 	  elsif (list[i].class == DamsDatastream::ListNote::Note)
+			Solrizer.insert_field(solr_doc, 'note_#{i}_value', list[i].value.first)		
+																
+		  end		  
+		  i +=1
+		end   
+	end 
           			
  # hack to strip "+00:00" from end of dates, because that makes solr barf
     ['system_create_dtsi','system_modified_dtsi'].each { |f|
