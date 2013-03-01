9368a56f790abe0459353480b522ea314339a94b
VivianChu <tchu@ucsd.edu>
2013-03-01 13:46:45 -0800

    Adding DamsScientificName model

---
+++ app/models/datastreams/dams_scientific_name_datastream.rb
@@ -0,0 +1,15 @@
+class DamsScientificNameDatastream < DamsDatastream
+  map_predicates do |map|
+    map.name(:in => MADS, :to => 'authoritativeLabel')
+    map.authority(:in => DAMS, :to => 'authority')
+    map.valURI(:in => DAMS, :to => 'valueURI')
+    map.elementList(:in => MADS, :to => 'elementList', :class_name=>'List')
+  end
+    
+  rdf_subject { |ds| RDF::URI.new(Rails.configuration.id_namespace + ds.pid)}
+  def serialize
+    graph.insert([rdf_subject, RDF.type, DAMS.ScientificName]) if new?
+    super
+  end
+    
+end
