cf5be1a4c02f274f34bcc783b4d126d03c934e3a
VivianChu <tchu@ucsd.edu>
2013-03-08 11:35:59 -0800

    Adding ComplexSubject to DamsObject class

--- app/models/mads_complex_subject.rb
+++ app/models/mads_complex_subject.rb
@@ -1,4 +1,4 @@
 class MadsComplexSubject < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => MadsComplexSubjectDatastream 
-  delegate_to "damsMetadata", [:name, :elementList, :authority, :valueURI]
+  delegate_to "damsMetadata", [:name, :elementList, :authority, :valueURI, :componentList]
 end
