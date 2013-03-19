8e505457f867e14e2fd68e7489169444df2500e7
Esmé Cowles <escowles@ucsd.edu>
2013-03-19 19:28:54 -0400

    Switching to streaming I/O to support delivering files larger than RAM

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -1,12 +1,31 @@
 class FileController < ApplicationController
   def show
+    # load metadata
     asset = ActiveFedora::Base.find(params[:id], :cast=>true)
-    opts = {}
     ds = asset.datastreams[params[:ds]]
-    opts[:filename] = params["filename"] || asset.label
-    opts[:disposition] = 'inline'
-    opts[:type] = ds.mimeType
-    send_data ds.content, opts
-    return
+
+    # set headers
+    filename = params["filename"] || "#{params[:id]}#{params[:ds]}"
+    headers['Content-Disposition'] = "inline; filename=#{filename}"
+    headers['Content-Type'] = ds.mimeType || 'application/octet-stream'
+    headers['Last-Modified'] = ds.lastModifiedDate || Time.now.ctime.to_s
+    if ds.size
+      headers['Content-Length'] = ds.size.to_s
+    else
+      headers['Transfer-Encoding'] = 'chunked'
+    end
+
+    # see https://github.com/cul/active_fedora_streamable/blob/master/lib/active_fedora_streamable.rb
+
+    # stream data to client (does not work in webrick)
+    parms = { :pid => params[:id], :dsid => params[:ds], :finished=>false }
+    repo = ActiveFedora::Base.connection_for_pid(parms[:pid])
+    self.response_body = Enumerator.new do |blk|
+      repo.datastream_dissemination(parms) do |res|
+        res.read_body do |seg|
+          blk << seg
+        end
+      end
+    end
   end
 end
