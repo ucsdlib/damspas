88963cd531f5cf7118b74ca100ad8978c2a87a21
VivianChu <tchu@ucsd.edu>
2013-03-22 14:54:32 -0700

    Moving Relationship class

--- app/models/datastreams/dams_resource_datastream.rb
+++ app/models/datastreams/dams_resource_datastream.rb
@@ -72,37 +72,7 @@ class DamsResourceDatastream < ActiveFedora::RdfxmlRDFDatastream
   
 
   ## Relationship ##############################################################
-  class Relationship
-    include ActiveFedora::RdfObject
-    rdf_type DAMS.Relationship
-    map_predicates do |map|
-      map.name(:in=> DAMS)
-      map.personalName(:in=> DAMS)
-      map.corporateName(:in=> DAMS)
-      map.role(:in=> DAMS)
-    end
 
-    def load
-      if name.first.to_s.start_with?(Rails.configuration.id_namespace)
-        md = /\/(\w*)$/.match(name.first.to_s)
-        MadsPersonalName.find(md[1])
-      elsif personalName.first.to_s.start_with?(Rails.configuration.id_namespace)
-        md = /\/(\w*)$/.match(personalName.first.to_s)
-        MadsPersonalName.find(md[1])
-      elsif corporateName.first.to_s.start_with?(Rails.configuration.id_namespace)
-        md = /\/(\w*)$/.match(corporateName.first.to_s)
-        MadsCorporateName.find(md[1])
-      end
-    end
-    
-    def loadRole
-      uri = role.first.to_s
-      if uri.start_with?(Rails.configuration.id_namespace)
-        md = /\/(\w*)$/.match(uri)
-        DamsRole.find(md[1])
-      end
-    end    
-  end
 
 
   ## Language ##################################################################
