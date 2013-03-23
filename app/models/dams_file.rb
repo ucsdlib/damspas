07d477962e5c654127d8ef06ea36041808b1b742
Esmé Cowles <escowles@ucsd.edu>
2013-03-23 18:32:58 -0400

    Moving File class to separate file

---
+++ app/models/dams_file.rb
@@ -0,0 +1,43 @@
+  class DamsFile
+    include ActiveFedora::RdfObject
+    rdf_type DAMS.File
+    map_predicates do |map|
+      map.filestore(:in=>DAMS)
+      map.quality(:in=>DAMS)
+      map.size(:in=>DAMS)
+      map.sourceFileName(:in=>DAMS)
+      map.sourcePath(:in=>DAMS)
+      map.use(:in=>DAMS)
+      map.value(:in=> RDF)
+
+      # checksums
+      map.crc32checksum(:in=>DAMS)
+      map.md5checksum(:in=>DAMS)
+      map.sha1checksum(:in=>DAMS)
+      map.sha256checksum(:in=>DAMS)
+      map.sha512checksum(:in=>DAMS)
+
+      # premis
+      map.compositionLevel(:in=>DAMS)
+      map.dateCreated(:in=>DAMS)
+      map.formatName(:in=>DAMS)
+      map.formatVersion(:in=>DAMS)
+      map.mimeType(:in=>DAMS)
+      map.objectCategory(:in=>DAMS)
+      map.preservationLevel(:in=>DAMS)
+      map.event(:in=>DAMS)
+
+      # mix
+      map.source_capture(:in=>DAMS, :to => 'sourceCapture')
+    end
+    def id
+      fid = rdf_subject.to_s
+      fid = fid.gsub(/.*\//,'')
+      fid
+    end
+    def order
+      order = id.gsub(/\..*/,'')
+      order.to_i
+    end
+  end
+
