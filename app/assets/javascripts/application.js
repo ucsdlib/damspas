6559063525ed37020ef891f259d5c052b2fffd6b
david <david@bilbo.local>
2013-03-27 15:11:59 -0700

    Updated COV component tree rendering. Arrow icon now toggles child container and links toggle component display. Also fixed toggle display bugs.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,18 +29,18 @@ Blacklight.do_search_context_behavior = function() {};
 //----------------------
 // <OBJECT VIEWER PAGE>
 //----------------------
-var componentloaded = [];
+
+var componentLoaded = [];
 function showComponent(componentIndex)
 {
 	var componentID = "#component-" + componentIndex;
 	var container = componentID + " > div";
-	var buttonID = "#tree-button-" + componentIndex;
+	var buttonID = "#node-btn-" + componentIndex;
 	var componentData = $(container).attr("data");
 
-	// Dynamically load images, audio files, and video files
-	if (componentData != undefined)
+	if (componentData != undefined) // Dynamically load images, audio files, and video files
 	{
-		if (!componentloaded[componentIndex])
+		if (!componentLoaded[componentIndex])
 		{
 			var controlID = "control-" + componentIndex;
 			componentData = $.parseJSON(componentData);
@@ -63,7 +63,7 @@ function showComponent(componentIndex)
 					break;
 			}
 		}
-		componentloaded[componentIndex] = true;
+		componentLoaded[componentIndex] = true;
 	}
 
 	// Highlight component tree button text of selected component
@@ -74,11 +74,39 @@ function showComponent(componentIndex)
 	$('.component').hide();
 	$(componentID).show();
 }
+
 $(document).ready(function()
 {
-	// Toggle parent arrows on component tree
-	$("[id^=meta-]").on("show",function(){$(this).prev().find("i").removeClass("icon-chevron-right").addClass("icon-chevron-down");});
-	$("[id^=meta-]").on("hide",function(){$(this).prev().find("i").removeClass("icon-chevron-down").addClass("icon-chevron-right");});
+	// Ensure all parent component containers are collapsed
+	$('.node-container').hide();
+
+	// Toggle parent component containers
+	$(".node-toggle").on("click",function()
+	{
+		if($(this).hasClass('icon-chevron-right')){
+			$(this).removeClass('icon-chevron-right').addClass('icon-chevron-down');
+			$(this).next().next().show();
+		}
+		else{
+			$(this).removeClass('icon-chevron-down').addClass('icon-chevron-right');
+			$(this).next().next().hide();
+		}
+	});
+
+	// Show/hide all components by clicking on sidebar header
+	$("#sidebar-header").on("click",function()
+	{
+		if($(this).hasClass('tree-collapsed')){
+			$(this).removeClass('tree-collapsed').addClass('tree-expanded');
+			$(".node-toggle").removeClass('icon-chevron-right').addClass('icon-chevron-down');
+			$('.node-container').show();
+		}
+		else{
+			$(this).removeClass('tree-expanded').addClass('tree-collapsed');
+			$(".node-toggle").removeClass('icon-chevron-down').addClass('icon-chevron-right');
+			$('.node-container').hide();
+		}
+	});
 
 	// Toggle metadata fold
 	$("#metadata-fold").on("show",function(){$(this).prev().text("Hide metadata");});
@@ -88,6 +116,7 @@ $(document).ready(function()
 	var firstWithFiles = $(".component[data]").attr("data");
 	showComponent(firstWithFiles)
 });
+
 //-----------------------
 // </OBJECT VIEWER PAGE>
 //-----------------------
