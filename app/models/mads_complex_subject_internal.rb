74c08fc6428ee919a14e3414d8ce530c3fead666
VivianChu <tchu@ucsd.edu>
2013-04-10 10:16:21 -0700

    Updating the subject internal class

--- app/models/mads_complex_subject_internal.rb
+++ app/models/mads_complex_subject_internal.rb
@@ -16,4 +16,15 @@ class MadsComplexSubjectInternal
       rdf_subject.to_s.gsub(/.*\//,'')
   end
 
+  def external?
+    rdf_subject.to_s.include? Rails.configuration.id_namespace
+  end
+    
+  def load
+      uri = rdf_subject.to_s
+      if uri.start_with?(Rails.configuration.id_namespace)
+        md = /\/(\w*)$/.match(uri)
+        MadsComplexSubject.find(md[1])
+      end
+  end
 end
