aff7e46db1b16328c71f4caf738b1fc3151bfb63
Esmé Cowles <escowles@ucsd.edu>
2013-04-18 15:08:37 -0400

    Adding new permission/restriction classes

---
+++ app/models/dams_permission.rb
@@ -0,0 +1,9 @@
+class DamsPermission
+    include ActiveFedora::RdfObject
+    rdf_type DAMS.Permission
+    map_predicates do |map|
+      map.type(:in=>DAMS)
+      map.beginDate(:in=>DAMS)
+      map.endDate(:in=>DAMS)
+    end
+end
