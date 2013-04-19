85ad45ce2a75c8e06e9f9d69b9a0bc8317e3670c
Esmé Cowles <escowles@ucsd.edu>
2013-04-19 10:29:36 -0400

    Using get_single_doc_via_search when only one record is needed

--- app/controllers/dams_collections_controller.rb
+++ app/controllers/dams_collections_controller.rb
@@ -2,7 +2,7 @@ class DamsCollectionsController < ApplicationController
   include Blacklight::Catalog
 
   def show
-    @response, @document = get_search_results( :q => "id:#{params[:id]}" )
+    @document = get_single_doc_via_search(1, {:q => "id:#{params[:id]}"} )
   end
   def index
     @response, @document = get_search_results( :q => "type_tesim:'Collection'", :rows => '50', :sort => 'col_name_ssi asc' )
