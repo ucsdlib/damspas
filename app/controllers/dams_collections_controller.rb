a2dfb133c082c517b64bada9032611f050b736f8
Esmé Cowles <escowles@ucsd.edu>
2013-04-19 10:06:52 -0400

    Updating solr calls

--- app/controllers/dams_collections_controller.rb
+++ app/controllers/dams_collections_controller.rb
@@ -2,9 +2,9 @@ class DamsCollectionsController < ApplicationController
   include Blacklight::Catalog
 
   def show
-    @response, @document = get_solr_response_for_doc_id
+    @response, @document = get_search_results( :q => "id:#{params[:id]}" )
   end
   def index
-    @response, @document = get_solr_response_for_field_values 'type_tesim', 'Collection', {:rows => '50', :sort => 'col_name_ssi asc'}
+    @response, @document = get_search_results( :q => "type_tesim:'Collection'", :rows => '50', :sort => 'col_name_ssi asc' )
   end
 end
