6a0a9929c79cb30501f7b49003c5e67121871225
VivianChu <tchu@ucsd.edu>
2013-04-26 14:43:59 -0700

    Adding MadsTemporal controller, view, create and edit pages

--- config/routes.rb
+++ config/routes.rb
@@ -74,6 +74,9 @@ Hydra::Application.routes.draw do
   resources :mads_topics do
     get 'view', :on => :member
   end  
+  resources :mads_temporals do
+    get 'view', :on => :member
+  end    
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
