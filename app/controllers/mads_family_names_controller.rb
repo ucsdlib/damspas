33b3ae64c60957cebf6fa45b67f70d0b62901d88
VivianChu <tchu@ucsd.edu>
2013-04-29 13:33:44 -0700

    DHH-68 - Adding MadsFamilyName controller, view, create and edit pages

---
+++ app/controllers/mads_family_names_controller.rb
@@ -0,0 +1,60 @@
+class MadsFamilyNamesController < ApplicationController
+  include Blacklight::Catalog
+  load_and_authorize_resource
+  skip_load_and_authorize_resource :only => [:index, :show]
+
+  ##############################################################################
+  # solr actions ###############################################################
+  ##############################################################################
+  def show
+    parm={ :q => "id_t:#{params[:id]}" }
+    @document = get_single_doc_via_search(1,parm)
+    @current_family_name = @document['name_tesim']
+    #@carousel_resp, @carousel = get_search_results( :q => "title_tesim:carousel AND id_t:#{params[:id]}", :qt=>"standard")
+     @carousel_resp, @carousel = get_search_results( :q => "title_tesim:carousel")
+  end
+  def index
+    # hydra index
+    #@mads_family_names = MadFamilyName.all( :order=>"system_create_dtsi asc" )
+
+    # solr index
+    @response, @document = get_search_results(:q => 'has_model_ssim:"info:fedora/afmodel:MadsFamilyName" AND NOT name_tesim:"family Name Test" AND NOT name_tesim:"family Name" AND NOT name_tesim:"Test Name"' )
+    @carousel_resp, @carousel = get_search_results( :q => "title_tesim:carousel")
+  end
+
+  ##############################################################################
+  # hydra actions ##############################################################
+  ##############################################################################
+  def view
+    @mads_family_name = MadsFamilyName.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def edit
+    #@mads_family_name = MadsFamilyName.find(params[:id])
+  end
+
+  def create
+    @mads_family_name.attributes = params[:mads_family_name]
+    if @mads_family_name.save
+        redirect_to @mads_family_name, notice: "family_name has been saved"
+    else
+      flash[:alert] = "Unable to save family_name"
+      render :new
+    end
+  end
+
+  def update
+    @mads_family_name.attributes = params[:mads_family_name]
+    if @mads_family_name.save
+        redirect_to @mads_family_name, notice: "Successfully updated family_name"
+    else
+      flash[:alert] = "Unable to save family_name"
+      render :edit
+    end
+  end
+
+end
