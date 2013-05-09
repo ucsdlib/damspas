bd95066e31b63f1b3f775e1331430583c00096c9
Esmé Cowles <escowles@ucsd.edu>
2013-05-09 10:13:16 -0400

    Updating units controller to redirect to hydra/view instead of show after updates

--- app/controllers/dams_units_controller.rb
+++ app/controllers/dams_units_controller.rb
@@ -9,6 +9,7 @@ class DamsUnitsController < ApplicationController
   def show
     parm={ :q => "unit_code_tesim:#{params[:id]} AND type_tesim:DamsUnit" }
     @document = get_single_doc_via_search(1,parm)
+logger.warn "solr: #{@document.inspect}"
     @current_unit = @document['unit_name_tesim']
     @carousel_resp, @carousel = get_search_results( :q => "title_tesim:carousel AND unit_code_tesim:#{params[:id]}", :qt=>"standard")
   end
@@ -39,7 +40,7 @@ class DamsUnitsController < ApplicationController
   def create
     @dams_unit.attributes = params[:dams_unit]
     if @dams_unit.save
-        redirect_to @dams_unit, notice: "Unit has been saved"
+        redirect_to view_dams_unit_path @dams_unit, notice: "Unit has been saved"
     else
       flash[:alert] = "Unable to save unit"
       render :new
@@ -49,7 +50,7 @@ class DamsUnitsController < ApplicationController
   def update
     @dams_unit.attributes = params[:dams_unit]
     if @dams_unit.save
-        redirect_to @dams_unit, notice: "Successfully updated unit"
+        redirect_to view_dams_unit_path @dams_unit, notice: "Successfully updated unit"
     else
       flash[:alert] = "Unable to save unit"
       render :edit
