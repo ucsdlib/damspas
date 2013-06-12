2fac8c91a7a290349ba87b13a601098e3be58ad4
Huawei Weng <wenghuawei@yahoo.com>
2013-06-12 15:45:18 -0700

    Updating provenance collection model class and view for the form

--- app/models/dams_provenance_collection.rb
+++ app/models/dams_provenance_collection.rb
@@ -1,7 +1,7 @@
 class DamsProvenanceCollection < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => DamsProvenanceCollectionDatastream 
-  delegate_to "damsMetadata", [:title, :date, :subject, :languageValue, :unit, :note, :scopeContentNote, :relatedResource, :titleValue, :titleType, :dateValue, :beginDate, :endDate, :scopeContentNoteType, :scopeContentNoteDisplayLabel, :scopeContentNoteValue, :subjectValue]
-  def part
+  delegate_to "damsMetadata", [:title, :date, :subject, :languageValue, :unit, :note, :scopeContentNote, :relatedResource, :titleValue, :titleType, :dateValue, :beginDate, :endDate, :scopeContentNoteType, :scopeContentNoteDisplayLabel, :scopeContentNoteValue, :subjectValue,:subjectType, :subjectTypeValue]
+ def part
     damsMetadata.load_part
   end
 
