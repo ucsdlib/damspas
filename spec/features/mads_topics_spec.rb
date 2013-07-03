c9ba6ade647facb94ae685cecdddfd25890423a2
Chris Ngo <ctngo.93@gmail.com>
2013-07-03 12:34:46 -0700

    Restructured MADS topics test to account for more topics

--- spec/features/mads_topics_spec.rb
+++ spec/features/mads_topics_spec.rb
@@ -1,72 +1,72 @@
 require 'spec_helper'
 
-feature 'Visitor wants to create a topic' do 
-	scenario 'is on new mads topic page' do
+# Variable to be used to store MADS topic path
+# Used for editing specified topic
+@@path = nil
+
+feature 'Visitor wants to create/edit a topic' do 
+	scenario 'is on mads index page' do
 		sign_in_developer
+		# Change to create button
 		visit "mads_topics/new"
 		# Create new topic
 		fill_in "Name", :with => "TestLabel"
 		fill_in "ExternalAuthority", :with => "http://test.com"
-		fill_in "TopicElement", :with => "Element"
+		fill_in "TopicElement", :with => "TestElement"
 		page.select('Test Scheme', match: :first) 
 		click_on "Submit"
 
-		# On TestLabel topic page
+		# Save path of topic for other test(s)
+		@@path = current_path
 		expect(page).to have_content ("TestLabel")
 		expect(page).to have_content ("http://test.com")
-		expect(page).to have_content ("Element")
+		expect(page).to have_content ("TestElement")
 		expect(page).to have_content ("Test Scheme")
+		expect(page).to have_content ("http://library.ucsd.edu/ark:/20775/")
 
-		# Check index page for newly created topic
-		visit mads_topics_path
-		expect(page).to have_content ("TestLabel")
-		all('a').select {|topic| topic.text == "TestLabel" }.last.click
-		
-		# Testing view
-		expect(page).to have_content ("TestLabel")
-		expect(page).to have_content ("http://test.com")
-		expect(page).to have_content ("Element")
-	end
-end
-
-feature 'Visitor wants to edit a topic' do
-	scenario 'is on mads topic page' do
-		sign_in_developer
-		visit mads_topics_path
-
-		# Click on first "TestLabel" topic name
-		all('a').select {|topic| topic.text == "TestLabel" }.last.click
 		expect(page).to have_selector('a', :text => "Edit")
 		click_on "Edit"
-		# Make changes
-		fill_in "Authoritative Label", :with => "Edited Test Topic"
-		fill_in "ExternalAuthority", :with => "http://edited.edu"
-		fill_in "TopicElement", :with => "Element2"
+		fill_in "Authoritative Label", :with => "Edit after Create"
+		fill_in "ExternalAuthority", :with => "http://editaftercreate.edu"
+		fill_in "TopicElement", :with => "Test Element2"
 		page.select('Test Scheme 2', match: :first) 
 		click_on "Save changes"
 
 		# Check that changes are saved
-		visit mads_topics_path
-		expect(page).to have_content ("Edited Test Topic")
-		all('a').select {|topic| topic.text == "Edited Test Topic" }.last.click
-		expect(page).to have_content ("Edited Test Topic")
-		expect(page).to have_content ("http://edited.edu")
-		expect(page).to have_content ("Element2")
-		expect(page).to have_content ("Test Scheme 2")
+		expect(page).to have_content("Edit after Create")
+		expect(page).to have_content("http://editaftercreate.edu")
+		expect(page).to have_content("Test Element2")
+		expect(page).to have_content("Test Scheme 2")
+		expect(page).to have_content ("http://library.ucsd.edu/ark:/20775/")
+	end
+
+	scenario 'is on the topic page to be edited' do
+		sign_in_developer
+		visit @@path
+		click_on "Edit"
+		fill_in "Authoritative Label", :with => "Edited Test Topic"
+		fill_in "ExternalAuthority", :with => "http://edited.edu"
+		fill_in "TopicElement", :with => "Test Element"
+		page.select('Test Scheme', match: :first) 
+		click_on "Save changes"
+		expect(page).to have_content("Edited Test Topic")
+		expect(page).to have_content("http://edited.edu")
+		expect(page).to have_content("Test Element")
+		expect(page).to have_content("Test Scheme")
+		expect(page).to have_content ("http://library.ucsd.edu/ark:/20775/")
 	end
 end
 
 feature 'Visitor wants to cancel unsaved edits' do
 	scenario 'is on Edit Topic page' do
 		sign_in_developer
-		visit mads_topics_path
-		all('a').select {|topic| topic.text == "Edited Test Topic" }.last.click
+		visit @@path
 		expect(page).to have_selector('a', :text => "Edit")
 		click_on "Edit"
 		fill_in "Authoritative Label", :with => "CANCEL"
 		fill_in "ExternalAuthority", :with => "http://cancel.edu"
 		fill_in "TopicElement", :with => "Should not show"
-		select("Test Scheme 2", :from => "scheme")
+		page.select('Test Scheme 2', match: :first) 
 		click_on "Cancel"
 		expect(page).to_not have_content("Should not show")
 		expect(page).to have_content("Edited Test Topic")
