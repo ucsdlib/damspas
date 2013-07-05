4fc50b5101a73821066beb2451e8f2dee13cf437
VivianChu <tchu@ucsd.edu>
2013-07-05 11:36:44 -0700

    Adding more fields to MadsPersonal models, spec and dams_helper. DHH-73

--- app/models/mads_personal_name.rb
+++ app/models/mads_personal_name.rb
@@ -1,5 +1,5 @@
 class MadsPersonalName < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => MadsPersonalNameDatastream 
-  delegate_to "damsMetadata", [:name, :elementList, :scheme, :externalAuthority]
+  delegate_to "damsMetadata", [:name, :elementList, :scheme, :externalAuthority, :fullNameValue, :familyNameValue, :givenNameValue, :dateNameValue, :nameLabel]
   
 end
