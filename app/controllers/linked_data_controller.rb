edd054a489d69c146fa32e9fbb5afcf94ad03490
VivianChu <tchu@ucsd.edu>
2013-07-09 13:01:56 -0700

    Adding new function to return the Name objects based on the type for RelationshipName drop down menu. DHH-92

--- app/controllers/linked_data_controller.rb
+++ app/controllers/linked_data_controller.rb
@@ -27,7 +27,26 @@ class LinkedDataController < ApplicationController
 		render :layout => false
 	end
   end
-  
+
+  def get_name 	
+  	#http://localhost:3000/linked_data/get_name/get_name?q=PersonalName
+  	if(!params[:q].nil? && params[:q] != '' && params[:q] == 'CorporateName')
+		@names = MadsCorporateName.all( :order=>"system_create_dtsi asc" )
+  	elsif(!params[:q].nil? && params[:q] != '' && params[:q] == 'PersonalName')
+		@names = MadsPersonalName.all( :order=>"system_create_dtsi asc" )	
+  	elsif(!params[:q].nil? && params[:q] != '' && params[:q] == 'Name')
+		@names = MadsName.all( :order=>"system_create_dtsi asc" )		
+  	elsif(!params[:q].nil? && params[:q] != '' && params[:q] == 'ConferenceName')
+		@names = MadsConferenceName.all( :order=>"system_create_dtsi asc" )		
+  	elsif(!params[:q].nil? && params[:q] != '' && params[:q] == 'FamilyName')
+		@names = MadsFamilyName.all( :order=>"system_create_dtsi asc" )					
+	else
+		@names = MadsCorporateName.all( :order=>"system_create_dtsi asc" )
+	end
+	
+	render :layout => false
+  end
+    
   def show
 	redirect_to :action => 'get_data'
   end
