c3b7d1208fb5cb921f12a8e59e71cb44ca030280
Esmé Cowles <escowles@ucsd.edu>
2013-07-09 11:34:07 -0400

    Adding second-step derivative generation, better file upload handling and error reporting (DHH-103)

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -38,10 +38,68 @@ class FileController < ApplicationController
   end
 
   def create
+    @id = params[:id]
     file = params[:file] if params[:file].respond_to?(:original_filename)
-    @obj = ActiveFedora::Base.find(params[:id], :cast=>true)
-    @obj.add_file( file, params[:ds], file.original_filename )
+    if file.nil?
+      flash[:alert] = "No file upload found"
+      redirect_to view_dams_object_path @id
+      return
+    end
+
+    @ds = "1" # TODO list ds and get next ds #
+    ext = File.extname(file.original_filename)
+    @ds += ext unless ext.nil?
+    @obj = ActiveFedora::Base.find(@id, :cast=>true)
+    @obj.add_file( file, @ds, file.original_filename )
     @obj.save!
-    redirect_to view_dams_object_path @obj, notice: "File Uploaded"
+    flash[:notice] = "File Uploaded"
+
+    # if a file is audio, image or video, render view with link to generate
+    # derivatives, otherwise, just go back to the object view
+    mt = file.content_type
+    if !mt.include?("audio") && !mt.include?("image") && !mt.include?("video")
+      redirect_to view_dams_object_path @obj, notice: "File Uploaded"
+    end
+  end
+  def deriv
+    begin
+      @obj = params[:id]
+      dspart = params[:ds].split("_")
+      if ( dspart.length == 2 )
+        @cid = nil
+        @fid = dspart[1]
+      elsif ( dspart.length == 3 )
+        @cid = dspart[1]
+        @fid = dspart[2]
+      else
+        redirect_to view_dams_object_path @obj, notice: "Invalid datastream name: #{params[:ds]}"
+      end
+
+      # add any extension stripped by rails
+      ext = File.extname(request.fullpath)
+      @fid += ext unless ext.nil?
+
+      user = ActiveFedora.fedora_config.credentials[:user]
+      pass = ActiveFedora.fedora_config.credentials[:password]
+      baseurl = ActiveFedora.fedora_config.credentials[:url]
+      baseurl = baseurl.gsub(/\/fedora$/,'')
+      url = "#{baseurl}/api/files/#{@obj}/"
+      url += "#{@cid}/" unless @cid.nil?
+      url += "#{@fid}/derivatives?format=json"
+      response = RestClient::Request.new(
+        :method => :post, :url => url, :user => user, :password => pass
+      ).execute
+      json = JSON.parse(response.to_str)
+      if json['status'] == 'OK'
+        flash[:notice] = json['message']
+      else
+        flash[:alert] = json['message']
+      end
+      redirect_to view_dams_object_path @obj
+    rescue Exception => e
+      logger.warn "Error generating derivatives #{e.to_s}"
+      flash[:alert] = e.to_s
+      redirect_to view_dams_object_path @obj
+    end
   end
 end
