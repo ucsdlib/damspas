31a3beb2ee7c7560dab0600771545a0c840da804
VivianChu <tchu@ucsd.edu>
2013-07-09 13:10:52 -0700

    Removing the unused code from the javascript for the DamsObject form. DHH-92

--- app/assets/javascripts/edit-object.js
+++ app/assets/javascripts/edit-object.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
-	
-	  $('#dams_object_subjectType_').change(function() {
-	  	  var selected_value = $("#dams_object_subjectType_").val();
-	      
-	      var fl = "";
-	      var q = "*";
-	      if(selected_value == "Geographic"){
-	      	fl = "suggest51";
-	      } else {
-	      	fl = "suggestall";
-	      }
-	      alert("changeeee" + selected_value + "-fl:"+fl); 
-	      $.get("/linked_data/get_data?fl="+fl+"&q="+q,function(data,status){
-		    alert("Data: " + data + "\nStatus: " + status);
-		  });
-	    });
-
 	  $('#dams_object_relationshipNameType_').change(function() {
 	  	  var q = $("#dams_object_relationshipNameType_").val();
 	      $.get("/linked_data/get_name/get_name?q="+q,function(data,status){
