f902f18d41eaa94f418163c11a36ba286c078f28
Huawei Weng <wenghuawei@yahoo.com>
2013-07-09 17:35:35 -0700

    Adding multiple fields to model and views for provenance collection part

--- app/models/dams_provenance_collection_part.rb
+++ app/models/dams_provenance_collection_part.rb
@@ -1,4 +1,5 @@
 class DamsProvenanceCollectionPart < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => DamsProvenanceCollectionPartDatastream 
-  delegate_to "damsMetadata", [:title, :date, :subject, :note, :scopeContentNote, :relatedResource, :titleValue, :dateValue, :beginDate, :endDate, :scopeContentNoteType, :scopeContentNoteDisplayLabel, :scopeContentNoteValue]
-end
+   delegate_to "damsMetadata", [:provenanceCollectionURI, :damsObjectURI, :relatedResourceType, :relatedResourceDescription, :relatedResourceUri,:languageURI, :scopeContentNoteType, :scopeContentNoteDisplayLabel, :scopeContentNoteValue, :noteValue, :noteType, :noteDisplayLabel, :relationshipName,  :title, :titleValue, :subtitle, :titlePartName, :titlePartNumber, :titleNonSort,:typeOfResource, :date, :dateValue, :beginDate, :endDate, :subject, :topic, :component, :file, :relatedResource, :language, :unit, :note, :sourceCapture, :subjectValue, :subjectURI, :unitURI, :subjectType, :subjectTypeValue ]
+
+ end
