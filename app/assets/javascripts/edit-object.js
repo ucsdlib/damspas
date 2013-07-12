2059f917bd5553915399fb5469f5f6bb9531e01c
Huawei Weng <wenghuawei@yahoo.com>
2013-07-12 10:25:30 -0700

    Adding functions to edit javascript

--- app/assets/javascripts/edit-object.js
+++ app/assets/javascripts/edit-object.js
@@ -7,5 +7,13 @@ $(document).ready(function() {
 	    $('#relationshipName').html(data);
 	  });	      
     });
+
+  $('#dams_provenance_collection_relationshipNameType_').change(function() {
+  	  var q = $("#dams_provenance_collection_relationshipNameType_").val(); 
+
+      $.get(baseURL+"/get_name/get_name?formType=dams_provenance_collection&q="+q,function(data,status){
+	    $('#relationshipName').html(data);
+	  });	      
+    });
 	    	    
 });
