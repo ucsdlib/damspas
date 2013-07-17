735a5b150bb36fa96b84941dca0aaeb2b243a3e3
Huawei Weng <wenghuawei@yahoo.com>
2013-07-17 16:17:45 -0700

    Adding multiple fields to show page,Updating controller.(DHH-86)

--- app/models/dams_provenance_collection.rb
+++ app/models/dams_provenance_collection.rb
@@ -1,7 +1,6 @@
 class DamsProvenanceCollection < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => DamsProvenanceCollectionDatastream 
-  delegate_to "damsMetadata", [:provenanceCollectionPartURI, :damsObjectURI, :relatedResourceType, :relatedResourceDescription, :relatedResourceUri,:languageURI, :scopeContentNoteType, :scopeContentNoteDisplayLabel, :scopeContentNoteValue, :noteValue, :noteType, :noteDisplayLabel, :relationshipName,  :title, :titleValue, :subtitle, :titlePartName, :titlePartNumber, :titleNonSort,:typeOfResource, :date, :dateValue, :beginDate, :endDate, :subject, :topic, :component, :file, :relatedResource, :language, :unit, :note, :sourceCapture, :subjectValue, :subjectURI, :unitURI, :subjectType, :subjectTypeValue,:relationshipRoleURI, :relationshipNameURI, :relationshipNameType, :relationshipNameValue]
-
+  delegate_to "damsMetadata", [:provenanceCollectionPartURI, :damsObjectURI, :relatedResourceType, :relatedResourceDescription, :relatedResourceUri,:languageURI, :scopeContentNoteType, :scopeContentNoteDisplayLabel, :scopeContentNoteValue, :noteValue, :noteType, :noteDisplayLabel, :relationshipName,  :title, :titleValue, :subtitle, :titlePartName, :titlePartNumber, :titleNonSort,:typeOfResource, :date, :dateValue, :beginDate, :endDate, :subject, :topic, :component, :file, :relatedResource, :language, :unit, :note, :sourceCapture, :subjectValue, :subjectURI, :unitURI, :subjectType, :subjectTypeValue,:relationshipRoleURI, :relationshipNameURI, :relationshipNameType, :relationshipNameValue, :object]
  def part
     damsMetadata.load_part 
   end
@@ -12,6 +11,10 @@ class DamsProvenanceCollection < ActiveFedora::Base
 
   def objects
     damsMetadata.loadObjects damsMetadata.object, DamsObject
+  end
+
+  def complexSubjects
+    damsMetadata.load_complexSubjects damsMetadata.subject
   end
   
 end
