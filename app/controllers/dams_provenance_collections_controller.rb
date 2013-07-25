39f95c224626b8354e13a395d55b18c3310bd1b4
Huawei Weng <wenghuawei@yahoo.com>
2013-07-25 16:53:25 -0700

    Updating provenanceCollection controller to load solr doc in show method

--- app/controllers/dams_provenance_collections_controller.rb
+++ app/controllers/dams_provenance_collections_controller.rb
@@ -10,8 +10,30 @@ class DamsProvenanceCollectionsController < ApplicationController
   
 
   def show
-    @dams_provenance_collection = DamsProvenanceCollection.find(params[:id])
-   end
+    # check ip for unauthenticated users
+    if current_user == nil
+      current_user = User.anonymous(request.ip)
+    end
+
+    @document = get_single_doc_via_search(1, {:q => "id:#{params[:id]}"} )
+    if @document.nil?
+      raise ActionController::RoutingError.new('Not Found')
+    end
+
+    @rdfxml = @document['rdfxml_ssi']
+    if @rdfxml == nil
+      @rdfxml = "<rdf:RDF xmlns:dams='http://library.ucsd.edu/ontology/dams#'
+          xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+          rdf:about='#{Rails.configuration.id_namespace}#{params[:id]}'>
+  <dams:error>content missing</dams:error>
+</rdf:RDF>"
+    end
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @document }
+      format.rdf { render xml: @rdfxml }
+    end
+  end
 
   def new
     
