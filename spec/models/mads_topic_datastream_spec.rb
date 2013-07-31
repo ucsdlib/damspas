2299acce99bbe832cb1dcd11ffebac1106520f12
Esmé Cowles <escowles@ucsd.edu>
2013-07-31 10:03:07 -0400

    Broken mads_topic_datastream test

--- spec/models/mads_topic_datastream_spec.rb
+++ spec/models/mads_topic_datastream_spec.rb
@@ -3,52 +3,48 @@ require 'spec_helper'
 
 describe MadsTopicDatastream do
 
-  describe "a complex data model" do
+  describe "nested attributes" do
+    it "should create a xml" do
+      exturi = RDF::Resource.new "http://id.loc.gov/authorities/subjects/sh85124118"
+      scheme = RDF::Resource.new "http://library.ucsd.edu/ark:/20775/bd9386739x"
+      params = {
+        topic: {
+          label: "Socialism", externalAuthority: exturi,
+          elementList_attributes: [
+            topicElement_attributes: [{ elementValue: "Socialism" }]
+          ],
+          scheme_attributes: [
+            pid: scheme, code: "lcsh", name: "Library of Congress Subject Headings"
+          ]
+        }
+      }
 
-    describe "a new instance" do
-      subject { MadsTopicDatastream.new(double('inner object', :pid=>'bbXXXXXXXXX23', :new? =>true), 'damsMetadata') }
-      it "should have a subject" do
-        subject.rdf_subject.to_s.should == "#{Rails.configuration.id_namespace}bbXXXXXXXXX23"
-      end
+      subject = MadsTopicDatastream.new( RDF::Graph.new )
+      subject.attributes = params[:topic]
 
-      it "should have a name" do
-        subject.name = "Baseball"
-        subject.name.should == ["Baseball"]
-      end
-      it "should have scheme" do
-        subject.scheme = "bd9386739x"
-        subject.scheme.to_s.should == "#{Rails.configuration.id_namespace}bd9386739x"
-      end
-    end
-
-    describe "an instance with content" do
-      subject do
-        subject = MadsTopicDatastream.new(double('inner object', :pid=>'bd46424836', :new? =>true), 'damsMetadata')
-        subject.content = File.new('spec/fixtures/madsTopic.rdf.xml').read
-        subject
-      end
-
-      it "should have name" do
-        subject.name.should == ["Baseball"]
-      end
-
-      it "should have an scheme" do
-        subject.scheme.to_s.should == "#{Rails.configuration.id_namespace}bd9386739x"
-      end
-
-      it "should have fields" do
-        list = subject.elementList.first
-        list[0].should be_kind_of MadsTopicDatastream::List::TopicElement
-        list[0].elementValue.should == ["Baseball"]
-        list.size.should == 1
-      end
-
-      it "should have a fields from solr doc" do
-        solr_doc = subject.to_solr
-        solr_doc["topic_element_tesim"].should == ["Baseball"]
-        solr_doc["scheme_tesim"].should == ["#{Rails.configuration.id_namespace}bd9386739x"]
-        solr_doc["scheme_name_tesim"].should == ["Library of Congress Subject Headings"]
-      end
+      xml =<<END
+<rdf:RDF
+  xmlns:mads="http://www.loc.gov/mads/rdf/v1#"
+  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#">
+ <mads:Topic rdf:about="#{Rails.configuration.id_namespace}zzXXXXXXX1">
+    <mads:authoritativeLabel>Socialism</mads:authoritativeLabel>
+    <mads:hasExactExternalAuthority rdf:resource="http://id.loc.gov/authorities/subjects/sh85124118"/>
+    <mads:isMemberOfMADSScheme>
+      <mads:MADSScheme rdf:about="#{Rails.configuration.id_namespace}bd9386739x">
+        <rdfs:label>Library of Congress Subject Headings</rdfs:label>
+        <mads:code>lcsh</mads:code>
+      </mads:MADSScheme>
+    </mads:isMemberOfMADSScheme>
+    <mads:elementList rdf:parseType="Collection">
+      <mads:TopicElement>
+        <mads:elementValue>Socialism</mads:elementValue>
+      </mads:TopicElement>
+    </mads:elementList>
+  </mads:Topic>
+</rdf:RDF>
+END
+      subject.content.should be_equivalent_to xml
     end
   end
 end
