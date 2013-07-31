6f8d250a710f782074c76d3b94347b5c5435cfa3
Esmé Cowles <escowles@ucsd.edu>
2013-07-31 10:36:40 -0400

    Updating tests for mads_topic and mads_topic_datastream, removing patch to active fedora termproxy

--- spec/models/mads_topic_spec.rb
+++ spec/models/mads_topic_spec.rb
@@ -3,32 +3,46 @@ require 'spec_helper'
 
 describe MadsTopic do
   subject do
-    MadsTopic.new pid: "zzXXXXXXX1"
+    MadsTopic.new pid: 'zzXXXXXXX1'
   end
-  it "should create a xml" do    
-    subject.name = "Baseball"
-    subject.scheme = "bd9386739x"
-    subject.externalAuthority =  "http://id.loc.gov/authorities/subjects/sh85012026"
-    subject.elementValue = "Baseball 2"
+  it "should create a xml" do
+    exturi = RDF::Resource.new "http://id.loc.gov/authorities/subjects/sh85124118"
+    scheme = RDF::Resource.new "http://library.ucsd.edu/ark:/20775/bd9386739x"
+    params = {
+      topic: {
+        label: "Socialism", externalAuthority: exturi,
+        elementList_attributes: [
+          topicElement_attributes: [{ elementValue: "Socialism" }]
+        ],
+        scheme_attributes: [
+          id: scheme, code: "lcsh", name: "Library of Congress Subject Headings"
+        ]
+      }
+    }
+    subject.damsMetadata.attributes = params[:topic]
+
     xml =<<END
 <rdf:RDF
-  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
   xmlns:mads="http://www.loc.gov/mads/rdf/v1#"
-  xmlns:owl="http://www.w3.org/2002/07/owl#"
-  xmlns:dams="http://library.ucsd.edu/ontology/dams#">
+  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#">
  <mads:Topic rdf:about="#{Rails.configuration.id_namespace}zzXXXXXXX1">
-    <mads:authoritativeLabel>Baseball</mads:authoritativeLabel>
-    <mads:hasExactExternalAuthority rdf:resource="http://id.loc.gov/authorities/subjects/sh85012026"/>
-    <mads:isMemberOfMADSScheme rdf:resource="#{Rails.configuration.id_namespace}bd9386739x"/>
+    <mads:authoritativeLabel>Socialism</mads:authoritativeLabel>
+    <mads:hasExactExternalAuthority rdf:resource="http://id.loc.gov/authorities/subjects/sh85124118"/>
+    <mads:isMemberOfMADSScheme>
+      <mads:MADSScheme rdf:about="#{Rails.configuration.id_namespace}bd9386739x">
+        <rdfs:label>Library of Congress Subject Headings</rdfs:label>
+        <mads:code>lcsh</mads:code>
+      </mads:MADSScheme>
+    </mads:isMemberOfMADSScheme>
     <mads:elementList rdf:parseType="Collection">
       <mads:TopicElement>
-        <mads:elementValue>Baseball 2</mads:elementValue>
+        <mads:elementValue>Socialism</mads:elementValue>
       </mads:TopicElement>
-    </mads:elementList>    
+    </mads:elementList>
   </mads:Topic>
 </rdf:RDF>
 END
     subject.damsMetadata.content.should be_equivalent_to xml
-
   end
 end
