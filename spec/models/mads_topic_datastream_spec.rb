8460913a76a639d06bb7f7388b1e083f22cd3cfb
Esmé Cowles <escowles@ucsd.edu>
2013-07-31 10:36:40 -0400

    Updating tests for mads_topic and mads_topic_datastream, removing patch to active fedora termproxy

--- spec/models/mads_topic_datastream_spec.rb
+++ spec/models/mads_topic_datastream_spec.rb
@@ -46,5 +46,51 @@ describe MadsTopicDatastream do
 END
       subject.content.should be_equivalent_to xml
     end
+    describe "a new instance" do
+      subject { MadsTopicDatastream.new(double('inner object', :pid=>'bbXXXXXXXXX23', :new? =>true), 'damsMetadata') }
+      it "should have a subject" do
+        subject.rdf_subject.to_s.should == "#{Rails.configuration.id_namespace}bbXXXXXXXXX23"
+      end
+
+      it "should have a name" do
+        subject.name = "Baseball"
+        subject.name.should == ["Baseball"]
+      end
+      it "should have scheme" do
+        subject.scheme = "bd9386739x"
+        subject.scheme.to_s.should == "#{Rails.configuration.id_namespace}bd9386739x"
+      end
+    end
+
+    describe "an instance with content" do
+      subject do
+        subject = MadsTopicDatastream.new(double('inner object', :pid=>'bd46424836', :new? =>true), 'damsMetadata')
+        subject.content = File.new('spec/fixtures/madsTopic.rdf.xml').read
+        subject
+      end
+
+      it "should have name" do
+        subject.name.should == ["Baseball"]
+      end
+
+      it "should have an scheme" do
+        subject.scheme.to_s.should == "#{Rails.configuration.id_namespace}bd9386739x"
+      end
+
+      it "should have fields" do
+        list = subject.elementList.first
+        list[0].should be_kind_of MadsTopicDatastream::List::TopicElement
+        list[0].elementValue.should == ["Baseball"]
+        list.size.should == 1
+      end
+
+      it "should have a fields from solr doc" do
+        solr_doc = subject.to_solr
+        solr_doc["topic_element_tesim"].should == ["Baseball"]
+        solr_doc["scheme_tesim"].should == ["#{Rails.configuration.id_namespace}bd9386739x"]
+        solr_doc["scheme_name_tesim"].should == ["Library of Congress Subject Headings"]
+      end
+    end
+
   end
 end
