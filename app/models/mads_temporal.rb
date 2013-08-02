5fa99951e43f1b0d15915134456ac480bed033fe
VivianChu <tchu@ucsd.edu>
2013-08-01 17:41:06 -0700

    Adding Temporal model validation and feature tests

--- app/models/mads_temporal.rb
+++ app/models/mads_temporal.rb
@@ -1,8 +1,8 @@
 class MadsTemporal < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => MadsTemporalDatastream 
-  delegate_to 'damsMetadata', [:name, :elementList, :scheme, :externalAuthority, :elementList_attributes, :scheme_attributes]
+  delegate_to 'damsMetadata', [:name, :elementList, :scheme, :externalAuthority, :elementList_attributes, :scheme_attributes, :label]
   
   #validates_length_of :name, :minimum => 2
-  #validates :name, :presence => true
+  validates :label, :presence => true
   #validates_presence_of :name
 end
