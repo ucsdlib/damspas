d65cda548dd2e2ba8a17ed86b3ce8c0e3dfa767a
Esmé Cowles <escowles@ucsd.edu>
2013-08-05 10:15:49 -0400

    Moving MADS Topic definition into a module

--- app/models/mads_topic_internal.rb
+++ app/models/mads_topic_internal.rb
@@ -1,18 +1,4 @@
 class MadsTopicInternal
   include ActiveFedora::RdfObject
-  include ActiveFedora::Rdf::DefaultNodes
-  include DamsHelper
-  rdf_type MADS.Topic
-  map_predicates do |map|
-    map.name(:in => MADS, :to => 'authoritativeLabel')
-    map.scheme(:in => MADS, :to => 'isMemberOfMADSScheme')
-    map.externalAuthorityNode(:in => MADS, :to => 'hasExactExternalAuthority')
-    map.elementList(:in => MADS, :to => 'elementList', :class_name=>'List')
-  end 
-  rdf_subject { |ds| RDF::URI.new(Rails.configuration.id_namespace + ds.pid)}
-  
-  def pid
-      rdf_subject.to_s.gsub(/.*\//,'')
-  end
-
+  include Dams::MadsTopic
 end
