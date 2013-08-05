a7038f7201cf27a1e751ccbfc303387932ac81c4
Esmé Cowles <escowles@ucsd.edu>
2013-08-05 10:29:31 -0400

    Breaking out core MADS functionality into separate module

--- lib/dams/mads_element.rb
+++ lib/dams/mads_element.rb
@@ -1,5 +1,6 @@
 module Dams
   module MadsElement
+    # = MADS Element, extended by MADS TopicElement, NameElement, etc.
     extend ActiveSupport::Concern
     include ActiveFedora::RdfObject
     included do
