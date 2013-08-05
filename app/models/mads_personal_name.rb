c1770284fa4b3ca27e8f95922df596fd78f34302
Esmé Cowles <escowles@ucsd.edu>
2013-08-05 14:05:42 -0400

    Adding PersonalName nested attributes class

--- app/models/mads_personal_name.rb
+++ app/models/mads_personal_name.rb
@@ -1,4 +1,4 @@
 class MadsPersonalName < ActiveFedora::Base
-  has_metadata 'damsMetadata', :type => MadsPersonalNameDatastream 
-  delegate_to "damsMetadata", [:name, :elementList, :scheme, :externalAuthority, :fullNameValue, :fullNameElement_attributes, :fullNameElement, :familyNameValue, :familyNameElement_attributes, :familyNameElement, :givenNameValue, :givenNameElement_attributes, :givenNameElement, :dateNameValue, :dateNameElement_attributes, :dateNameElement, :termsOfAddressNameValue, :termsOfAddressNameElement_attributes, :termsOfAddressNameElement, :nameValue, :nameElement_attributes, :nameElement, :scheme_attributes]
+  has_metadata 'damsMetadata', :type => MadsPersonalNameDatastream
+  delegate_to 'damsMetadata', [:name, :scheme, :elementList, :externalAuthority, :scheme_attributes, :nameElement, :nameElement_attributes, :givenNameElement, :givenNameElement_attributes, :fullNameElement, :fullNameElement_attributes, :familyNameElement, :familyNameElement_attributes, :dateNameElement, :dateNameElement_attributes, :termsOfAddressNameElement, :termsOfAddressNameElement_attributes ]
 end
