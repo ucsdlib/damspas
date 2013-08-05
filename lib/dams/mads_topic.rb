c4260d386a4584f27d093c4ea81479a92a93ae72
Esmé Cowles <escowles@ucsd.edu>
2013-08-05 10:29:31 -0400

    Breaking out core MADS functionality into separate module

--- lib/dams/mads_topic.rb
+++ lib/dams/mads_topic.rb
@@ -3,22 +3,13 @@ require 'active_support/concern'
 module Dams
   module MadsTopic
     extend ActiveSupport::Concern
+    include Dams::MadsSimpleType
     included do
       rdf_type MADS.Topic
-      rdf_subject { |ds| RDF::URI.new(Rails.configuration.id_namespace + ds.pid)}
-      map_predicates do |map|
-        map.name(:in => MADS, :to => 'authoritativeLabel')
-        map.externalAuthority(:in => MADS, :to => 'hasExactExternalAuthority')
-        map.scheme(:in => MADS, :to => 'isMemberOfMADSScheme', :class_name => 'MadsSchemeInternal')
-        map.elem_list(:in => MADS, :to => 'elementList', :class_name=>'MadsTopicElementList')
-      end
       accepts_nested_attributes_for :topicElement, :scheme
       def serialize
         graph.insert([rdf_subject, RDF.type, MADS.Topic]) if new?
         super
-      end
-      def elementList
-        elem_list.first || elem_list.build
       end
       delegate :topicElement_attributes=, to: :elementList
       alias_method :topicElement, :elementList
@@ -35,14 +26,7 @@ module Dams
         if elementList.first
           Solrizer.insert_field(solr_doc, "topic_element", elementList.first.elementValue.to_s)
         end
-        Solrizer.insert_field(solr_doc, 'name', name)
-        if scheme.first
-          Solrizer.insert_field(solr_doc, 'scheme', scheme.first.rdf_subject.to_s)
-          Solrizer.insert_field(solr_doc, 'scheme_name', scheme.first.name.first)
-          Solrizer.insert_field(solr_doc, 'scheme_code', scheme.first.code.first)
-        end
-        Solrizer.insert_field(solr_doc, "externalAuthority", externalAuthority.first.to_s)
-        solr_doc
+        solr_base solr_doc
       end
       class MadsTopicElementList
         include ActiveFedora::RdfList
