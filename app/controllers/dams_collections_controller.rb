ea6b905ca1840a7c6b5d479a8457bd98ca37127c
Huawei Weng <wenghuawei@yahoo.com>
2013-08-14 12:33:42 -0700

    Adding parsing code for nested part to mixed collection list. (DHH-155)

--- app/controllers/dams_collections_controller.rb
+++ app/controllers/dams_collections_controller.rb
@@ -5,7 +5,7 @@ class DamsCollectionsController < ApplicationController
     @document = get_single_doc_via_search(1, {:q => "id:#{params[:id]}"} )
   end
   def index
-    @response, @document = get_search_results( {:q => "type_tesim:'Collection'", :rows => '100', :sort => 'title_ssi asc'}, {:fq => "-id:#{Rails.configuration.excluded_collections}"} )
+    @response, @document = get_search_results( {:q => "type_tesim:'Collection'", :rows => '200', :sort => 'title_ssi asc'}, {:fq => "-id:#{Rails.configuration.excluded_collections}"} )
     @response, @ProvenanceDocument = get_search_results(:q => 'has_model_ssim:"info:fedora/afmodel:DamsProvenanceCollection"', :rows => 100, :sort => 'title_ssi asc' )
      @response, @AssembledDocument = get_search_results(:q => 'has_model_ssim:"info:fedora/afmodel:DamsAssembledCollection"', :rows => 100, :sort => 'title_ssi asc' )
      
