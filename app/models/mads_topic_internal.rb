6a6da00350cc43fa80f92b6c4541a8c32fe433aa
VivianChu <tchu@ucsd.edu>
2013-08-15 15:02:31 -0700

    Updating the ComplexSubject create form and spec. DHH-107

--- app/models/mads_topic_internal.rb
+++ app/models/mads_topic_internal.rb
@@ -1,7 +1,15 @@
 class MadsTopicInternal
   include ActiveFedora::RdfObject
+  include ActiveFedora::Rdf::DefaultNodes
   include Dams::MadsTopic
   def pid
-      rdf_subject.to_s.gsub(/.*\//,'')
+    rdf_subject.to_s.gsub(/.*\//,'')
+  end
+  # used by fields_for, so this ought to move to ActiveFedora if it works
+  def persisted?
+    rdf_subject.kind_of? RDF::URI
+  end
+  def id
+    rdf_subject if rdf_subject.kind_of? RDF::URI
   end
 end
