ddcbfb9b15b8b71120a9436cf67d33143c5e43f5
Esmé Cowles <escowles@ucsd.edu>
2013-08-28 10:50:04 -0400

    Solr indexing cleanup, renaming Component to DamsComponentInternal

--- app/models/dams_other_rights.rb
+++ app/models/dams_other_rights.rb
@@ -1,5 +1,5 @@
 class DamsOtherRights < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => DamsOtherRightsDatastream
-  delegate_to "damsMetadata", [:basis,:note,:uri,:permissionType, :permissionBeginDate, :permissionEndDate, :restrictionType, :restrictionBeginDate, :restrictionEndDate, :name, :role ]
+  delegate_to "damsMetadata", [:basis,:note,:uri,:permissionType, :permissionBeginDate, :permissionEndDate, :restrictionType, :restrictionBeginDate, :restrictionEndDate, :name, :role, :relationship ]
 # :decider
 end
