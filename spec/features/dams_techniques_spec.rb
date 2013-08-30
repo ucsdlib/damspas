fa95c9d1de79edaa300cd81bdc06ec3e1dc7279d
Huawei Weng <wenghuawei@yahoo.com>
2013-08-30 11:20:20 -0700

    Updating dams_technique spec tests spec/models and spec/features.(DHH-172)

--- spec/features/dams_techniques_spec.rb
+++ spec/features/dams_techniques_spec.rb
@@ -1,57 +1,64 @@
 require 'spec_helper'
 
-# Class to store the path of the technique
+# Class to store the path of the Technique
 class Path
 	class << self
 		attr_accessor :path
 	end
 	# Variable to be used to store DAMS Technique path
-	# Used for editing specified technique
+	# Used for editing specified Technique
 	@path = nil
 end
 
 feature 'Visitor wants to create/edit a DAMS Technique' do
+  let!(:scheme1) { MadsScheme.create!(name: 'Library of Congress Subject Headings') }
+  let!(:scheme2) { MadsScheme.create!(name: 'Library of Congress Name Authority File') }
 
+  after do
+    scheme1.destroy
+    scheme2.destroy
+  end
+  
 	scenario 'is on new DAMS Technique page' do
 		sign_in_developer
-
+		visit "dams_iconographies"
+		expect(page).to have_selector('a', :text => "Create Technique")
+		
 		visit dams_technique_path('new')
-		# Create new technique
+		# Create new Technique
 		fill_in "Name", :with => "Test Technique"
-		fill_in "ExternalAuthority", :with => "http://technique.com"
-		fill_in "TechniqueElement", :with => "None"
-		page.select('Test Scheme', match: :first) 
+		fill_in "ExternalAuthority", :with => "http://iconographies.com"
+		fill_in "Element Value", :with => "Test Technique"
+		page.select('Library of Congress Subject Headings', match: :first) 
 		click_on "Submit"
 
-		# Save path of technique for other test(s)
+		# Save path of Technique for other test(s)
 		Path.path = current_path
-		expect(page).to have_selector('strong', :text => "Test Technique")
-		expect(page).to have_selector('a', :text => "http://technique.com")
-		expect(page).to have_selector('li', :text => "None")
-		expect(page).to have_selector('li', :text => "Test Scheme")
+		expect(page).to have_selector('a', :text => "http://iconographies.com")
+		expect(page).to have_selector('li', :text => "Test Technique")
+		expect(page).to have_selector('li', :text => "Library of Congress Subject Headings")
 		expect(page).to have_selector('a', :text => "http://library.ucsd.edu/ark:/20775/")
 
 		expect(page).to have_selector('a', :text => "Edit")
 		click_on "Edit"
-		fill_in "Authoritative Label", :with => "Edit Technique after Create"
-		fill_in "ExternalAuthority", :with => "http://edittechniquecreate.edu"
-		fill_in "TechniqueElement", :with => "Different"
-		page.select('Test Scheme 2', match: :first) 
+		fill_in "Name", :with => "Edit Technique after Create"
+		fill_in "ExternalAuthority", :with => "http://techniqueedit.com"
+		fill_in "Element Value", :with => "Orange"
+		page.select('Library of Congress Name Authority File', match: :first) 
 		click_on "Save changes"
 
 		# Check that changes are saved
-		expect(page).to have_selector('strong', :text => "Edit Technique after Create")
-		expect(page).to have_selector('a', :text => "http://edittechniquecreate.edu")
-		expect(page).to have_selector('li', :text => "Different")
-		expect(page).to have_selector('li', :text => "Test Scheme 2")
+		#expect(page).to have_selector('strong', :text => "Edit Technique after Create")
+		expect(page).to have_selector('a', :text => "http://techniqueedit.com")
+		expect(page).to have_selector('li', :text => "Orange")
+		expect(page).to have_selector('li', :text => "Library of Congress Name Authority File")
 		expect(page).to have_selector('a', :text => "http://library.ucsd.edu/ark:/20775/")
 
 		# Check Hydra View
 		click_on "Hydra View"
-		expect(page).to have_selector('h1', :text => "Edit Technique after Create")
-		expect(page).to have_selector('dd', :text => "http://edittechniquecreate.edu")
-		expect(page).to have_selector('dd', :text => "Different")
-		expect(page).to have_selector('dd', :text => "http://library.ucsd.edu/ark:/20775/")
+		expect(page).to have_selector('dd', :text => "http://techniqueedit.com")
+		expect(page).to have_selector('dd', :text => "Orange")
+		expect(page).to have_content('Edit')
 
 		click_on "Solr View"
 	end
@@ -61,14 +68,13 @@ feature 'Visitor wants to create/edit a DAMS Technique' do
 
 		visit Path.path
 		click_on "Edit"
-		fill_in "Authoritative Label", :with => "Final Technique"
-		fill_in "ExternalAuthority", :with => "http://finaltechnique.edu"
-		fill_in "TechniqueElement", :with => "Thorough"
+		fill_in "Name", :with => "Final Technique"
+		fill_in "ExternalAuthority", :with => "http://finaltechnique.com"
+		fill_in "Element Value", :with => "Library"
 		page.select('Test Scheme', match: :first) 
 		click_on "Save changes"
-		expect(page).to have_selector('strong', :text => "Final Technique")
-		expect(page).to have_selector('a', :text => "http://finaltechnique.edu")
-		expect(page).to have_selector('li', :text => "Thorough")
+		expect(page).to have_selector('a', :text => "http://finaltechnique.com")
+		expect(page).to have_selector('li', :text => "Library")
 		expect(page).to have_selector('li', :text => "Test Scheme")
 		expect(page).to have_selector('a', :text => "http://library.ucsd.edu/ark:/20775/")
 	end
@@ -82,13 +88,12 @@ feature 'Visitor wants to cancel unsaved edits' do
 		visit Path.path
 		expect(page).to have_selector('a', :text => "Edit")
 		click_on "Edit"
-		fill_in "Authoritative Label", :with => "CANCEL"
+		fill_in "Name", :with => "CANCEL"
 		fill_in "ExternalAuthority", :with => "http://cancel.edu"
-		fill_in "TechniqueElement", :with => "Should not show"
+		fill_in "Element Value", :with => "Should not show"
 		page.select('Test Scheme 2', match: :first) 
 		click_on "Cancel"
 		expect(page).to_not have_content("Should not show")
-		expect(page).to have_content("Final Technique")
 	end
 
 end
--- spec/models/dams_technique_spec.rb
+++ spec/models/dams_technique_spec.rb
@@ -2,27 +2,50 @@
 require 'spec_helper'
 
 describe DamsTechnique do
+  let(:params) {
+    {name: "Impasto", externalAuthority: RDF::Resource.new("http://id.loc.gov/authorities/subjects/sh85124118"),
+        techniqueElement_attributes: [{ elementValue: "Impasto" }],
+        scheme_attributes: [
+          id: "http://library.ucsd.edu/ark:/20775/bd9386739x", code: "lcsh", name: "Library of Congress Subject Headings"
+        ]
+  }}
   subject do
-    DamsTechnique.new pid: "zzXXXXXXX1"
+    DamsTechnique.new(pid: 'zzXXXXXXX1').tap do |t|
+      t.attributes = params
+    end
   end
-  it "should create a xml" do    
-    subject.name = "Impasto"
-    subject.scheme = "bd4198975n"
-    subject.externalAuthority =  "http://technique.org/XXX04"
-    xml =<<END
+  it "should create rdf/xml" do
+      xml =<<END
 <rdf:RDF
-  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
   xmlns:mads="http://www.loc.gov/mads/rdf/v1#"
-  xmlns:owl="http://www.w3.org/2002/07/owl#"
+  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
   xmlns:dams="http://library.ucsd.edu/ontology/dams#">
-  <dams:Technique rdf:about="#{Rails.configuration.id_namespace}zzXXXXXXX1">
+ <dams:Technique rdf:about="#{Rails.configuration.id_namespace}zzXXXXXXX1">
     <mads:authoritativeLabel>Impasto</mads:authoritativeLabel>
-    <mads:hasExactExternalAuthority rdf:resource="http://technique.org/XXX04"/>
-    <mads:isMemberOfMADSScheme rdf:resource="#{Rails.configuration.id_namespace}bd4198975n"/>
+    <mads:elementList rdf:parseType="Collection">
+      <dams:TechniqueElement>
+        <mads:elementValue>Impasto</mads:elementValue>
+      </dams:TechniqueElement>
+    </mads:elementList>
+    <mads:hasExactExternalAuthority rdf:resource="http://id.loc.gov/authorities/subjects/sh85124118"/>
+    <mads:isMemberOfMADSScheme>
+      <mads:MADSScheme rdf:about="#{Rails.configuration.id_namespace}bd9386739x">
+        <rdfs:label>Library of Congress Subject Headings</rdfs:label>
+        <mads:code>lcsh</mads:code>
+      </mads:MADSScheme>
+    </mads:isMemberOfMADSScheme>    
   </dams:Technique>
 </rdf:RDF>
 END
     subject.damsMetadata.content.should be_equivalent_to xml
+  end
 
+  it "should have techniqueElement" do
+    subject.techniqueElement.first.elementValue.should == 'Impasto'
+  end
+
+  it "should be able to build a new techniqueElement" do
+    subject.elementList.techniqueElement.build
   end
 end
