03620d8f9955a5f0175baad82c9ae5d29566d5f8
Huawei Weng <wenghuawei@yahoo.com>
2013-08-30 11:20:20 -0700

    Updating dams_technique spec tests spec/models and spec/features.(DHH-172)

--- spec/models/dams_technique_datastream_spec.rb
+++ spec/models/dams_technique_datastream_spec.rb
@@ -2,9 +2,48 @@
 require 'spec_helper'
 
 describe DamsTechniqueDatastream do
+  describe "nested attributes" do
+    it "should create a xml" do
+      exturi = RDF::Resource.new "http://id.loc.gov/authorities/subjects/sh85124118"
+      scheme = RDF::Resource.new "http://library.ucsd.edu/ark:/20775/bd9386739x"
+      params = {
+        technique: {
+          name: "Impasto", externalAuthority: exturi,
+          techniqueElement_attributes: [{ elementValue: "Impasto" }],
+          scheme_attributes: [
+            id: scheme, code: "lcsh", name: "Library of Congress Subject Headings"
+          ]
+        }
+      }
 
-  describe "a complex data model" do
+      subject = DamsTechniqueDatastream.new(double("inner object", pid:"zzXXXXXXX1", new?: true))
+      subject.attributes = params[:technique]
 
+      xml =<<END
+<rdf:RDF
+  xmlns:mads="http://www.loc.gov/mads/rdf/v1#"
+  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+  xmlns:dams="http://library.ucsd.edu/ontology/dams#">
+ <dams:Technique rdf:about="#{Rails.configuration.id_namespace}zzXXXXXXX1">
+    <mads:authoritativeLabel>Impasto</mads:authoritativeLabel>
+    <mads:elementList rdf:parseType="Collection">
+      <dams:TechniqueElement>
+        <mads:elementValue>Impasto</mads:elementValue>
+      </dams:TechniqueElement>
+    </mads:elementList>
+    <mads:hasExactExternalAuthority rdf:resource="http://id.loc.gov/authorities/subjects/sh85124118"/>
+    <mads:isMemberOfMADSScheme>
+      <mads:MADSScheme rdf:about="#{Rails.configuration.id_namespace}bd9386739x">
+        <rdfs:label>Library of Congress Subject Headings</rdfs:label>
+        <mads:code>lcsh</mads:code>
+      </mads:MADSScheme>
+    </mads:isMemberOfMADSScheme>    
+  </dams:Technique>
+</rdf:RDF>
+END
+      subject.content.should be_equivalent_to xml
+    end
     describe "a new instance" do
       subject { DamsTechniqueDatastream.new(double('inner object', :pid=>'bbXXXXXXXXX23', :new? =>true), 'damsMetadata') }
       it "should have a subject" do
@@ -12,13 +51,20 @@ describe DamsTechniqueDatastream do
       end
 
       it "should have a name" do
-        subject.name = "Impasto"
-        subject.name.should == ["Impasto"]
-      end   
-      it "should have scheme" do
-        subject.scheme = "bd4198975n"
-        subject.scheme.to_s.should == "#{Rails.configuration.id_namespace}bd4198975n"
-      end          
+        subject.name = "Baseball"
+        subject.name.should == ["Baseball"]
+      end
+
+      it "should set the name (authoritativeLabel) when the elementList is set" do
+        subject.name = "Original"
+        subject.techniqueElement_attributes = {'0' => { elementValue: "Test" }}
+        subject.name.should == ["Test"]
+      end
+      it "shouldn't set the name when the elementList doesn't have an elementValue" do
+        subject.name = "Original"
+        subject.techniqueElement_attributes = [{ elementValue: nil }]
+        subject.name.should == ["Original"]
+      end
     end
 
     describe "an instance with content" do
@@ -27,34 +73,30 @@ describe DamsTechniqueDatastream do
         subject.content = File.new('spec/fixtures/damsTechnique.rdf.xml').read
         subject
       end
-      
-      
+
       it "should have name" do
         subject.name.should == ["Impasto"]
       end
- 
+
       it "should have an scheme" do
-        subject.scheme.to_s.should == "#{Rails.configuration.id_namespace}bd4198975n"
+        subject.scheme.first.pid.should == "bd9386739x"
       end
 
-      it "should have a externalAuthority" do
-        subject.externalAuthority.to_s.should == "http://id.loc.gov/XXX04"
-      end
-                      
       it "should have fields" do
-        list = subject.elementList.first
-        list[0].should be_kind_of DamsTechniqueDatastream::List::TechniqueElement
-        list[0].elementValue.should == ["Impasto"]       
-        list.size.should == 1       
-      end  
-      
+        list = subject.elementList
+        list[0].should be_kind_of Dams::DamsTechnique::DamsTechniqueElement
+        list[0].elementValue.should == "Impasto"
+        list.size.should == 1
+      end
+
       it "should have a fields from solr doc" do
         solr_doc = subject.to_solr
+        solr_doc["technique_tesim"].should == ["Impasto"]
         solr_doc["technique_element_tesim"].should == ["Impasto"]
-        solr_doc["name_tesim"].should == ["Impasto"]
-        solr_doc["scheme_tesim"].should == ["#{Rails.configuration.id_namespace}bd4198975n"]
-        solr_doc["externalAuthority_tesim"].should == ["http://id.loc.gov/XXX04"]
-      end    
+        solr_doc["scheme_tesim"].should == ["#{Rails.configuration.id_namespace}bd9386739x"]
+        solr_doc["scheme_name_tesim"].should == ["Technique"]
+      end
     end
+
   end
 end
