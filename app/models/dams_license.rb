5e4ef43ec835a2e8549739f92a7758eb3f4b64a8
VivianChu <tchu@ucsd.edu>
2013-09-03 12:49:34 -0700

    Adding nested_attributes DamsLicense class and updating the tests. DHH-174

--- app/models/dams_license.rb
+++ app/models/dams_license.rb
@@ -1,4 +1,5 @@
 class DamsLicense < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => DamsLicenseDatastream
-  delegate_to "damsMetadata", [:note,:uri,:permissionType, :permissionBeginDate, :permissionEndDate, :restrictionType, :restrictionBeginDate, :restrictionEndDate ]
+  delegate_to "damsMetadata", [:note,:uri,:permissionType, :permissionBeginDate, :permissionEndDate, :restrictionType, :restrictionBeginDate, :restrictionEndDate,
+  							   :permission_node, :permission_node_attributes, :restriction_node, :restriction_node_attributes ]
 end
