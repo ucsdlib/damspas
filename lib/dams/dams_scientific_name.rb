735bbc30776bab5ae04186f5a52d5d64521ede46
Huawei Weng <wenghuawei@yahoo.com>
2013-09-06 17:04:01 -0700

    Adding nested_attributes for dams_scientific_name and refactored model,spec testes.(DHH-170)

---
+++ lib/dams/dams_scientific_name.rb
@@ -0,0 +1,51 @@
+require 'active_support/concern'
+
+module Dams
+  module DamsScientificName
+    extend ActiveSupport::Concern
+    include Dams::MadsSimpleType
+    included do
+      rdf_type DAMS.ScientificName
+      map_predicates do |map|
+        map.elem_list(:in => MADS, :to => 'elementList', :class_name=>'DamsScientificNameElementList')
+      end
+
+      def elementList
+        elem_list.first || elem_list.build
+      end      
+      accepts_nested_attributes_for :scientificNameElement, :scheme
+      def serialize
+        graph.insert([rdf_subject, RDF.type, DAMS.ScientificName]) if new?
+        super
+      end
+      delegate :scientificNameElement_attributes=, to: :elementList
+      alias_method :scientificNameElement, :elementList
+      def scientificNameElement_with_update_name= (attributes)
+        self.scientificNameElement_without_update_name= attributes
+        if elementList && elementList.first && elementList.first.elementValue.present?
+          self.name = elementList.first.elementValue
+        end
+      end
+      alias_method :scientificNameElement_without_update_name=, :scientificNameElement_attributes=
+      alias_method :scientificNameElement_attributes=, :scientificNameElement_with_update_name=
+      def to_solr (solr_doc={})
+        Solrizer.insert_field(solr_doc, 'scientific_name', name)
+        if elementList.first
+          Solrizer.insert_field(solr_doc, "scientific_name_element", elementList.first.elementValue.to_s)
+        end
+        solr_base solr_doc
+      end
+    end
+    class DamsScientificNameElementList
+      include ActiveFedora::RdfList
+      map_predicates do |map|
+        map.scientificNameElement(:in=> DAMS, :to =>"ScientificNameElement", :class_name => "DamsScientificNameElement")
+      end
+      accepts_nested_attributes_for :scientificNameElement
+    end
+    class DamsScientificNameElement
+      include Dams::MadsElement
+      rdf_type DAMS.ScientificNameElement
+    end
+  end
+end
--- spec/models/dams_scientific_name_datastream_spec.rb
+++ spec/models/dams_scientific_name_datastream_spec.rb
@@ -2,9 +2,48 @@
 require 'spec_helper'
 
 describe DamsScientificNameDatastream do
+  describe "nested attributes" do
+    it "should create a xml" do
+      exturi = RDF::Resource.new "http://id.loc.gov/authorities/subjects/sh85124118"
+      scheme = RDF::Resource.new "http://library.ucsd.edu/ark:/20775/bd9386739x"
+      params = {
+        scientificName: {
+          name: "Western lowland gorilla (Gorilla gorilla gorilla)", externalAuthority: exturi,
+          scientificNameElement_attributes: [{ elementValue: "Western lowland gorilla (Gorilla gorilla gorilla)" }],
+          scheme_attributes: [
+            id: scheme, code: "lcsh", name: "Library of Congress Subject Headings"
+          ]
+        }
+      }
 
-  describe "a complex data model" do
+      subject = DamsScientificNameDatastream.new(double("inner object", pid:"zzXXXXXXX1", new?: true))
+      subject.attributes = params[:scientificName]
 
+      xml =<<END
+<rdf:RDF
+  xmlns:mads="http://www.loc.gov/mads/rdf/v1#"
+  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+  xmlns:dams="http://library.ucsd.edu/ontology/dams#">
+ <dams:ScientificName rdf:about="#{Rails.configuration.id_namespace}zzXXXXXXX1">
+    <mads:authoritativeLabel>Western lowland gorilla (Gorilla gorilla gorilla)</mads:authoritativeLabel>
+    <mads:elementList rdf:parseType="Collection">
+      <dams:ScientificNameElement>
+        <mads:elementValue>Western lowland gorilla (Gorilla gorilla gorilla)</mads:elementValue>
+      </dams:ScientificNameElement>
+    </mads:elementList>
+    <mads:hasExactExternalAuthority rdf:resource="http://id.loc.gov/authorities/subjects/sh85124118"/>
+    <mads:isMemberOfMADSScheme>
+      <mads:MADSScheme rdf:about="#{Rails.configuration.id_namespace}bd9386739x">
+        <rdfs:label>Library of Congress Subject Headings</rdfs:label>
+        <mads:code>lcsh</mads:code>
+      </mads:MADSScheme>
+    </mads:isMemberOfMADSScheme>    
+  </dams:ScientificName>
+</rdf:RDF>
+END
+      subject.content.should be_equivalent_to xml
+    end
     describe "a new instance" do
       subject { DamsScientificNameDatastream.new(double('inner object', :pid=>'bbXXXXXXXXX23', :new? =>true), 'damsMetadata') }
       it "should have a subject" do
@@ -12,13 +51,20 @@ describe DamsScientificNameDatastream do
       end
 
       it "should have a name" do
-        subject.name = "Western lowland gorilla (Gorilla gorilla gorilla)"
-        subject.name.should == ["Western lowland gorilla (Gorilla gorilla gorilla)"]
-      end   
-      it "should have scheme" do
-        subject.scheme = "bd6792855f"
-        subject.scheme.to_s.should == "#{Rails.configuration.id_namespace}bd6792855f"
-      end          
+        subject.name = "Baseball"
+        subject.name.should == ["Baseball"]
+      end
+
+      it "should set the name (authoritativeLabel) when the elementList is set" do
+        subject.name = "Original"
+        subject.scientificNameElement_attributes = {'0' => { elementValue: "Test" }}
+        subject.name.should == ["Test"]
+      end
+      it "shouldn't set the name when the elementList doesn't have an elementValue" do
+        subject.name = "Original"
+        subject.scientificNameElement_attributes = [{ elementValue: nil }]
+        subject.name.should == ["Original"]
+      end
     end
 
     describe "an instance with content" do
@@ -27,34 +73,30 @@ describe DamsScientificNameDatastream do
         subject.content = File.new('spec/fixtures/damsScientificName.rdf.xml').read
         subject
       end
-      
-      
+
       it "should have name" do
         subject.name.should == ["Western lowland gorilla (Gorilla gorilla gorilla)"]
       end
- 
+
       it "should have an scheme" do
-        subject.scheme.to_s.should == "#{Rails.configuration.id_namespace}bd6792855f"
+        subject.scheme.first.pid.should == "bd6792855f"
       end
 
-      it "should have a externalAuthority" do
-        subject.externalAuthority.to_s.should == "http://dbpedia.org/page/Western_lowland_gorilla"
-      end
-                      
       it "should have fields" do
-        list = subject.elementList.first
-        list[0].should be_kind_of DamsScientificNameDatastream::List::ScientificNameElement
-        list[0].elementValue.should == ["Western lowland gorilla (Gorilla gorilla gorilla)"]       
-        list.size.should == 1       
-      end  
-      
+        list = subject.elementList
+        list[0].should be_kind_of Dams::DamsScientificName::DamsScientificNameElement
+        list[0].elementValue.should == "Western lowland gorilla (Gorilla gorilla gorilla)"
+        list.size.should == 1
+      end
+
       it "should have a fields from solr doc" do
         solr_doc = subject.to_solr
-        solr_doc["scientificName_element_tesim"].should == ["Western lowland gorilla (Gorilla gorilla gorilla)"]
-        solr_doc["name_tesim"].should == ["Western lowland gorilla (Gorilla gorilla gorilla)"]
+        solr_doc["scientific_name_tesim"].should == ["Western lowland gorilla (Gorilla gorilla gorilla)"]
+        solr_doc["scientific_name_element_tesim"].should == ["Western lowland gorilla (Gorilla gorilla gorilla)"]
         solr_doc["scheme_tesim"].should == ["#{Rails.configuration.id_namespace}bd6792855f"]
-        solr_doc["externalAuthority_tesim"].should == ["http://dbpedia.org/page/Western_lowland_gorilla"]
-      end    
+        solr_doc["scheme_name_tesim"].should == ["Scientific Names"]
+      end
     end
+
   end
 end
