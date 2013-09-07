862bf2e7eddc17931552d513c8a6d53271974deb
Huawei Weng <wenghuawei@yahoo.com>
2013-09-06 17:04:01 -0700

    Adding nested_attributes for dams_scientific_name and refactored model,spec testes.(DHH-170)

--- app/models/dams_scientific_name.rb
+++ app/models/dams_scientific_name.rb
@@ -1,5 +1,5 @@
 class DamsScientificName < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => DamsScientificNameDatastream 
-  delegate_to "damsMetadata", [:name, :scheme, :externalAuthority, :elementList]
+  delegate_to "damsMetadata", [:name, :scheme, :elementList, :externalAuthority, :scientificNameElement_attributes, :scientificNameElement, :scheme_attributes]
   
 end
--- app/models/dams_scientific_name_internal.rb
+++ app/models/dams_scientific_name_internal.rb
@@ -1,18 +1,14 @@
 class DamsScientificNameInternal
     include ActiveFedora::RdfObject
-    include ActiveFedora::Rdf::DefaultNodes
-    include DamsHelper
-    rdf_type DAMS.ScientificName
-  rdf_subject { |ds| RDF::URI.new(Rails.configuration.id_namespace + ds.pid)}
-  map_predicates do |map|
-    map.name(:in => MADS, :to => 'authoritativeLabel')
-    map.scheme(:in => MADS, :to => 'isMemberOfMADSScheme')
-    map.externalAuthorityNode(:in => MADS, :to => 'hasExactExternalAuthority')
-    map.elementList(:in => MADS, :to => 'elementList', :class_name=>'List')
-  end 
-  
+  include Dams::DamsScientificName
+
   def pid
-      rdf_subject.to_s.gsub(/.*\//,'')
+    rdf_subject.to_s.gsub(/.*\//,'')
+  end
+  
+
+  def id
+    rdf_subject if rdf_subject.kind_of? RDF::URI
   end
 
 end
