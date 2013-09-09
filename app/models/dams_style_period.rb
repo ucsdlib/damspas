963336146b657c9381d060a049c79f62efc0dba6
Huawei Weng <wenghuawei@yahoo.com>
2013-09-09 16:07:03 -0700

    Adding nested_attributes for dams_style_period and refacotored model classes and spec tests.(DHH-171)

--- app/models/dams_style_period.rb
+++ app/models/dams_style_period.rb
@@ -1,5 +1,5 @@
 class DamsStylePeriod < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => DamsStylePeriodDatastream 
-  delegate_to "damsMetadata", [:name, :scheme, :externalAuthority, :elementList, :elementValue]
+  delegate_to "damsMetadata", [:name, :scheme, :elementList, :externalAuthority, :stylePeriodElement_attributes, :stylePeriodElement, :scheme_attributes]
   
 end
--- app/models/datastreams/dams_style_period_datastream.rb
+++ app/models/datastreams/dams_style_period_datastream.rb
@@ -1,22 +1,3 @@
-class DamsStylePeriodDatastream < DamsDatastream
-  include DamsHelper
-  map_predicates do |map|
-    map.name(:in => MADS, :to => 'authoritativeLabel')
-    map.schemeNode(:in => MADS, :to => 'isMemberOfMADSScheme')
-    map.externalAuthorityNode(:in => MADS, :to => 'hasExactExternalAuthority')
-    map.elementList(:in => MADS, :to => 'elementList', :class_name=>'List')
-  end
-    
-  rdf_subject { |ds| RDF::URI.new(Rails.configuration.id_namespace + ds.pid)}
-  def serialize
-    graph.insert([rdf_subject, RDF.type, DAMS.StylePeriod]) if new?
-    super
-  end
-  def elementValue
-    getElementValue "StylePeriodElement"
-  end
-  
-  def elementValue=(s)
-    setElementValue( "StylePeriodElement", s )
-  end  
+class DamsStylePeriodDatastream < ActiveFedora::RdfxmlRDFDatastream
+  include Dams::DamsStylePeriod
 end
