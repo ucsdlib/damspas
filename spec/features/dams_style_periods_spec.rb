cd06c4a7adf82c29262b2d485c9bcb7188488588
Huawei Weng <wenghuawei@yahoo.com>
2013-09-09 16:44:34 -0700

    Refactored dams_style_period controller,view,create/edit forms and feature tests for nested attributes.(DHH-171)

--- spec/features/dams_style_periods_spec.rb
+++ spec/features/dams_style_periods_spec.rb
@@ -1,75 +1,77 @@
 require 'spec_helper'
 
-# Class to store the path of the StylePeriod
+# Class to store the path of the style period
 class Path
 	class << self
 		attr_accessor :path
 	end
-	# Variable to be used to store DAMS StylePeriod path
-	# Used for editing specified StylePeriod
+	# Variable to be used to store DAMS style period path
+	# Used for editing specified style period
 	@path = nil
 end
 
-feature 'Visitor wants to create/edit a DAMS StylePeriod' do
+feature 'Visitor wants to create/edit a DAMS style period' do
+  let!(:scheme1) { MadsScheme.create!(name: 'Library of Congress Subject Headings') }
+  let!(:scheme2) { MadsScheme.create!(name: 'Library of Congress Name Authority File') }
 
-	scenario 'is on new DAMS StylePeriod page' do
+  after do
+    scheme1.destroy
+    scheme2.destroy
+  end
+  
+	scenario 'is on new DAMS style period page' do
 		sign_in_developer
-
+		visit "dams_style_periods"
+		
+		
 		visit dams_style_period_path('new')
-		# Create new StylePeriod
-		fill_in "Name", :with => "New StylePeriod"
-		fill_in "ExternalAuthority", :with => "http://firstperiod.com"
-		fill_in "StylePeriodElement", :with => "Temp"
-		page.select('Test Scheme', match: :first) 
+		# Create new style period
+		fill_in "Name", :with => "Test style period"
+		fill_in "ExternalAuthority", :with => "http://styleperiods.com"
+		fill_in "Element Value", :with => "Test style period"
+		page.select('Library of Congress Subject Headings', match: :first) 
 		click_on "Submit"
 
-		# Save path of StylePeriod for other test(s)
+		# Save path of style period for other test(s)
 		Path.path = current_path
-		expect(page).to have_selector('strong', :text => "New StylePeriod")
-		expect(page).to have_selector('a', :text => "http://firstperiod.com")
-		expect(page).to have_selector('li', :text => "Temp")
-		expect(page).to have_selector('li', :text => "Test Scheme")
+		expect(page).to have_selector('a', :text => "http://styleperiods.com")
+		expect(page).to have_selector('li', :text => "Library of Congress Subject Headings")
 		expect(page).to have_selector('a', :text => "http://library.ucsd.edu/ark:/20775/")
 
 		expect(page).to have_selector('a', :text => "Edit")
 		click_on "Edit"
-		fill_in "Authoritative Label", :with => "Edit StylePeriod after Create"
-		fill_in "ExternalAuthority", :with => "http://newperiod.com"
-		fill_in "StylePeriodElement", :with => "Old"
-		page.select('Test Scheme 2', match: :first) 
+		fill_in "Name", :with => "Edit style period after Create"
+		fill_in "ExternalAuthority", :with => "http://styleperiodedit.com"
+		fill_in "Element Value", :with => "Orange"
+		page.select('Library of Congress Name Authority File', match: :first) 
 		click_on "Save changes"
 
 		# Check that changes are saved
-		expect(page).to have_selector('strong', :text => "Edit StylePeriod after Create")
-		expect(page).to have_selector('a', :text => "http://newperiod.com")
-		expect(page).to have_selector('li', :text => "Old")
-		expect(page).to have_selector('li', :text => "Test Scheme 2")
+		
+		expect(page).to have_selector('a', :text => "http://styleperiodedit.com")
+		expect(page).to have_selector('li', :text => "Library of Congress Name Authority File")
 		expect(page).to have_selector('a', :text => "http://library.ucsd.edu/ark:/20775/")
 
 		# Check Hydra View
 		click_on "Hydra View"
-		expect(page).to have_selector('h1', :text => "Edit StylePeriod after Create")
-		expect(page).to have_selector('dd', :text => "http://newperiod.com")
-		expect(page).to have_selector('dd', :text => "Old")
-		expect(page).to have_selector('dd', :text => "http://library.ucsd.edu/ark:/20775/")
+		expect(page).to have_selector('dd', :text => "http://styleperiodedit.com")
+		expect(page).to have_selector('dd', :text => "Orange")
 		expect(page).to have_content('Edit')
 
 		click_on "Solr View"
 	end
 
-	scenario 'is on the StylePeriod page to be edited' do
+	scenario 'is on the style period page to be edited' do
 		sign_in_developer
 
 		visit Path.path
 		click_on "Edit"
-		fill_in "Authoritative Label", :with => "Test StylePeriod"
-		fill_in "ExternalAuthority", :with => "http://styleperiods.com"
-		fill_in "StylePeriodElement", :with => "Current"
+		fill_in "Name", :with => "Final style period"
+		fill_in "ExternalAuthority", :with => "http://finalstyleperiod.com"
+		fill_in "Element Value", :with => "Library"
 		page.select('Test Scheme', match: :first) 
 		click_on "Save changes"
-		expect(page).to have_selector('strong', :text => "Test StylePeriod")
-		expect(page).to have_selector('a', :text => "http://styleperiods.com")
-		expect(page).to have_selector('li', :text => "Current")
+		expect(page).to have_selector('a', :text => "http://finalstyleperiod.com")
 		expect(page).to have_selector('li', :text => "Test Scheme")
 		expect(page).to have_selector('a', :text => "http://library.ucsd.edu/ark:/20775/")
 	end
@@ -78,18 +80,17 @@ end
 
 feature 'Visitor wants to cancel unsaved edits' do
 	
-	scenario 'is on Edit StylePeriod page' do
+	scenario 'is on Edit style period page' do
 		sign_in_developer
 		visit Path.path
 		expect(page).to have_selector('a', :text => "Edit")
 		click_on "Edit"
-		fill_in "Authoritative Label", :with => "CANCEL"
+		fill_in "Name", :with => "CANCEL"
 		fill_in "ExternalAuthority", :with => "http://cancel.edu"
-		fill_in "StylePeriodElement", :with => "Should not show"
+		fill_in "Element Value", :with => "Should not show"
 		page.select('Test Scheme 2', match: :first) 
 		click_on "Cancel"
 		expect(page).to_not have_content("Should not show")
-		expect(page).to have_content("Test StylePeriod")
 	end
 
 end
