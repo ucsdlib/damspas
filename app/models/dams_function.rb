dba74ecaccf37b3a6cd5054f8947d855741e6f91
VivianChu <tchu@ucsd.edu>
2013-09-13 11:31:00 -0700

    Merged DamsFunction updates. DHH-113

--- app/models/dams_function.rb
+++ app/models/dams_function.rb
@@ -1,5 +1,5 @@
 class DamsFunction < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => DamsFunctionDatastream 
-  delegate_to "damsMetadata", [:name, :scheme, :externalAuthority, :elementList, :elementValue]
+ 	delegate_to "damsMetadata", [:name, :scheme, :elementList, :externalAuthority, :functionElement_attributes, :functionElement, :scheme_attributes]
   
 end
