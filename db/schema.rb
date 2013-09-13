351ff7842d19ce00bf4b173b602ad505754ce207
Huawei Weng <wenghuawei@yahoo.com>
2013-09-13 11:43:08 -0700

    Adding date type and encoding to assembled collection and provenance collection parts model and forms.(DHH-198)

--- db/schema.rb
+++ db/schema.rb
@@ -9,44 +9,44 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121210190838) do
+ActiveRecord::Schema.define(version: 20121210190838) do
 
-  create_table "bookmarks", :force => true do |t|
-    t.integer  "user_id",     :null => false
+  create_table "bookmarks", force: true do |t|
+    t.integer  "user_id",     null: false
     t.string   "document_id"
     t.string   "title"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "user_type"
   end
 
-  create_table "searches", :force => true do |t|
+  create_table "searches", force: true do |t|
     t.text     "query_params"
     t.integer  "user_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "user_type"
   end
 
-  add_index "searches", ["user_id"], :name => "index_searches_on_user_id"
+  add_index "searches", ["user_id"], name: "index_searches_on_user_id"
 
-  create_table "users", :force => true do |t|
-    t.string   "email",              :default => ""
-    t.string   "uid",                :default => "",    :null => false
-    t.string   "provider",           :default => "",    :null => false
-    t.integer  "sign_in_count",      :default => 0
+  create_table "users", force: true do |t|
+    t.string   "email",              default: ""
+    t.string   "uid",                default: "",    null: false
+    t.string   "provider",           default: "",    null: false
+    t.integer  "sign_in_count",      default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                            :null => false
-    t.datetime "updated_at",                            :null => false
-    t.boolean  "guest",              :default => false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.boolean  "guest",              default: false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["provider", "uid"], :name => "index_users_on_provider_and_uid", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
 
 end
