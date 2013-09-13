e21094d915213a53a2e15469029d6a3ea05c90de
Huawei Weng <wenghuawei@yahoo.com>
2013-09-13 10:36:31 -0700

    Adding date type and encoding to provenance collection edit forms, updating spec tests.(DHH-198)

--- app/models/dams_cultural_context_internal.rb
+++ app/models/dams_cultural_context_internal.rb
@@ -5,10 +5,7 @@ class DamsCulturalContextInternal
   def pid
     rdf_subject.to_s.gsub(/.*\//,'')
   end
-  # used by fields_for, so this ought to move to ActiveFedora if it works
-  def persisted?
-    rdf_subject.kind_of? RDF::URI
-  end
+  
 
   def id
     rdf_subject if rdf_subject.kind_of? RDF::URI
