670e1907cc37b49233f6c50596e6411396cc754a
Huawei Weng <wenghuawei@yahoo.com>
2013-09-13 15:39:15 -0700

    Adding nested attributes for dams_built_work_places and refactored MVC, spec tests and forms. (DHH-168)

--- app/models/dams_built_work_place.rb
+++ app/models/dams_built_work_place.rb
@@ -1,4 +1,6 @@
 class DamsBuiltWorkPlace < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => DamsBuiltWorkPlaceDatastream 
-  delegate_to "damsMetadata", [:name, :scheme, :externalAuthority, :elementList, :elementValue]
+  delegate_to "damsMetadata", [:name, :scheme, :elementList, :externalAuthority, :builtWorkPlaceElement_attributes, :builtWorkPlaceElement, :scheme_attributes]
+  
 end
+
--- app/models/datastreams/dams_built_work_place_datastream.rb
+++ app/models/datastreams/dams_built_work_place_datastream.rb
@@ -1,21 +1,3 @@
-class DamsBuiltWorkPlaceDatastream < DamsDatastream
-  include DamsHelper
-  map_predicates do |map|
-    map.name(:in => MADS, :to => 'authoritativeLabel')
-    map.schemeNode(:in => MADS, :to => 'isMemberOfMADSScheme')
-    map.externalAuthorityNode(:in => MADS, :to => 'hasExactExternalAuthority')
-    map.elementList(:in => MADS, :to => 'elementList', :class_name=>'List')
-  end
-  rdf_subject { |ds| RDF::URI.new(Rails.configuration.id_namespace + ds.pid)}
-  def serialize
-    graph.insert([rdf_subject, RDF.type, DAMS.BuiltWorkPlace]) if new?
-    super
-  end
-  def elementValue
-    getElementValue "BuiltWorkPlaceElement"
-  end
-  
-  def elementValue=(s)
-    setElementValue( "BuiltWorkPlaceElement", s )
-  end  
+class DamsBuiltWorkPlaceDatastream < ActiveFedora::RdfxmlRDFDatastream
+  include Dams::DamsBuiltWorkPlace
 end
