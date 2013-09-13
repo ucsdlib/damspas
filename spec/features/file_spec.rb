9760311978d9ea02e09f63438d02097d1275a035
Esmé Cowles <escowles@ucsd.edu>
2013-09-13 14:58:39 -0400

    Backing out solr-based access control enforcement b/c rights fields aren't getting indexed in solr

--- spec/features/file_spec.rb
+++ spec/features/file_spec.rb
@@ -6,25 +6,25 @@ require 'cancan'
 @obj = DamsObject.new
 @obj.attributes = {titleValue:"File Upload Test", copyrightURI: "bd0513099p"}
 @obj.save
-pid = @obj.pid
+test_pid = @obj.pid
 
-# reindex to make sure rights are index correctly
+# reindex to make sure rights are indexed correctly
 solrizer = Solrizer::Fedora::Solrizer.new
-solrizer.solrize pid
+solrizer.solrize test_pid
 
 feature "Anonymous user shouldn't be able to upload files" do
   scenario "Link to Hydra view should be hidden" do
-    visit dams_object_path(pid)
+    visit dams_object_path(test_pid)
     expect(page).not_to have_selector('a', :text => 'Hydra View')
   end
 
   scenario "Shouldn't be able to access Hydra view" do
-    expect { visit view_dams_object_path(pid) }.to raise_error(
+    expect { visit view_dams_object_path(test_pid) }.to raise_error(
       CanCan::AccessDenied)
   end
 
   scenario "Shouldn't be able to upload file" do
-    expect { page.driver.post upload_path(pid) }.to raise_error(
+    expect { page.driver.post upload_path(test_pid) }.to raise_error(
       CanCan::AccessDenied)
   end
 end
@@ -32,31 +32,31 @@ end
 feature "Curator should be able to upload files" do
   scenario "Link to Hydra view should be shown" do
     sign_in_developer
-    visit dams_object_path(pid)
+    visit dams_object_path(test_pid)
     expect(page).to have_selector('a', :text => 'Hydra View')
   end
   scenario "Empty upload should return error" do
     sign_in_developer
-    visit view_dams_object_path(pid)
+    visit view_dams_object_path(test_pid)
     expect(page).to have_selector('h2', :text => 'Add File')
     click_on "Add File"
     expect(page).to have_selector('div', :text => 'No file upload found')
   end
   scenario "Uploading XML file" do
     sign_in_developer
-    visit view_dams_object_path(pid)
+    visit view_dams_object_path(test_pid)
     attach_file 'file', File.join(Rails.root,'/spec/fixtures/madsScheme.rdf.xml')
     click_on "Add File"
     expect(page).to have_selector('div', :text => 'File Uploaded')
 
     # make sure the file actually exists
-    visit file_path( pid, '_1.xml' )
+    visit file_path( test_pid, '_1.xml' )
     response = page.driver.response
     expect(response.status).to eq( 200 )
   end
   scenario "Uploading JPG file and generating derivatives" do
     sign_in_developer
-    visit view_dams_object_path(pid)
+    visit view_dams_object_path(test_pid)
     attach_file 'file', File.join(Rails.root,'/app/assets/images/ucsd/bg.jpg')
     click_on "Add File"
     expect(page).to have_selector('div', :text => 'File Uploaded')
@@ -67,12 +67,12 @@ feature "Curator should be able to upload files" do
     expect(page).to have_selector('div', :text => 'Derivatives created successfully')
 
     # make sure file exists
-    visit file_path( pid, '_1.jpg' )
+    visit file_path( test_pid, '_1.jpg' )
     response = page.driver.response
     expect(response.status).to eq( 200 )
 
     # make sure derivative exists
-    visit file_path( pid, '_2.jpg' )
+    visit file_path( test_pid, '_2.jpg' )
     response = page.driver.response
     expect(response.status).to eq( 200 )
   end
@@ -84,6 +84,7 @@ feature "Access control enforcement" do
     expect(page.driver.response.status).to eq( 200 )
   end
   scenario "Anonymous shouldn't be able to access restricted object files" do
+    pending("access control enforcement")
     expect { visit file_path( 'bd0922518w', '_4_4.jpg' ) }.to raise_error(
       CanCan::AccessDenied)
   end
