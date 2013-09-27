c85ccb671d2f89a09a2cc51fa1fc10c316045abe
VivianChu <tchu@ucsd.edu>
2013-09-27 13:42:44 -0700

    Adding nested-attribute simple subjects to the create form. Updating models, controllers and javascript. DHH-203

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -10,7 +10,20 @@ function getSubjects(type,q,location,fieldName,label)
 {
   $.get(baseURL+"/get_subject/get_subject?label="+label+"&fieldName="+fieldName+"&formType="+type+"&q="+q,function(data,status){
     $(location).html(data);
-  });	 
+  });	
+
+  $('#simpleSubjects').show();
+  //$('#simpleSubject'+q).show();
+  //$('#simpleSubjectTemporal').hide();
+  //$("#simpleSubjectTopic").on("show",function(){$(this).prev().text("Hide Topic");});
+  //$("#simpleSubjectTopic").on("hide",function(){$(this).prev().text("Show Topic");});
+  
+  var subjectsArray =new Array("BuiltWorkPlace","CulturalContext","Function","GenreForm","Geographic","Iconography","Occupation","ScientificName","StylePeriod","Technique","Temporal","Topic");
+  for (var i in subjectsArray) {
+  	if(subjectsArray[i] == q) {
+	  $('#'+q).show();
+	}else {
+	  $('#'+subjectsArray[i]).hide();
+	}
+  }
 }
-
-
--- app/assets/javascripts/views-object.js
+++ app/assets/javascripts/views-object.js
@@ -236,6 +236,8 @@ $(document).ready(function()
 	$("#metadata-fold").on("show",function(){$(this).prev().text("Hide metadata");});
 	$("#metadata-fold").on("hide",function(){$(this).prev().text("Show metadata");});
 
+	$('#simpleSubjects').hide();
+	
 	// Display the first component with a file (denoted by: data='componentIndex') if present
 	if ($(".component[data]").attr("data") != undefined)
 	{
