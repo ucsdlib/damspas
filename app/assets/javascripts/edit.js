1d5f05077759370df2a5b5ece6c1b419f0bf5692
VivianChu <tchu@ucsd.edu>
2013-10-08 15:54:37 -0700

    Updating the link_to_add_fields function. DHH-104

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -131,10 +131,6 @@ function setLanguageId() {
   });
 }
 
-function remove_fields(link) {
-  $(link).prev("input[type=hidden]").val("1");
-  $(link).closest(".fields").hide();
-}
 
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
