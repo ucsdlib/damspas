37e959d9ec9c21bfdb3ab903ef6e96fc2025d429
VivianChu <tchu@ucsd.edu>
2013-10-09 14:22:56 -0700

    Adding the dynamic controll to remove Note field to DamsObject create form. DHH-104

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -107,8 +107,8 @@ result << resource.name.humanize.titleize
 end
  
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
-    #f.hidden_field(:_destroy) + link_to_function name, "remove_fields(this)"
+    #f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+    link_to_function name, "remove_fields(this)"
   end
   
   def link_to_add_fields(name, f, association, type)
