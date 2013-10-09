eeba8297ef7fcda2e76694ca08f64330fa65d000
Esmé Cowles <escowles@ucsd.edu>
2013-10-09 13:49:48 -0400

    Adding format facet to collection records (DHH-167)

--- app/models/datastreams/dams_assembled_collection_datastream.rb
+++ app/models/datastreams/dams_assembled_collection_datastream.rb
@@ -27,9 +27,11 @@ class DamsAssembledCollectionDatastream < DamsResourceDatastream
   end  
   
   def to_solr (solr_doc = {})
+    facetable = Solrizer::Descriptor.new(:string, :indexed, :multivalued)
     Solrizer.insert_field(solr_doc, 'type', 'Collection')
     Solrizer.insert_field(solr_doc, 'type', 'AssembledCollection')
     Solrizer.insert_field(solr_doc, 'resource_type', format_name(resource_type))
+    Solrizer.insert_field(solr_doc, 'object_type', format_name(resource_type),facetable)
     Solrizer.insert_field(solr_doc, 'visibility', visibility)
     
     part = load_part 
