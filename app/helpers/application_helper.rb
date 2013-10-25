d95d90dc7e248654b26a3643bb2fde5aff15e82a
david <david@naruto.local>
2013-10-25 13:03:03 -0700

    Updating 'Editor Tools' menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,7 +72,7 @@ module ApplicationHelper
   
   # render page titles for all application pages using Digital Library Collections prefix
   def full_title(page_title)
-    base_title = "Digital Collections"
+    base_title = 'Digital Collections - UC San Diego Library'
     if(page_title.blank?)
       base_title
     else
