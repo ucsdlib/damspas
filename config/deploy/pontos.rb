fce77385308aece8b3a9adaf99061f6d850c5537
Esmé Cowles <escowles@ucsd.edu>
2013-10-29 11:18:47 -0400

    Adding optional support for setting capistrano ssh keys using an environment variable (different for each env)

--- config/deploy/pontos.rb
+++ config/deploy/pontos.rb
@@ -1,3 +1,7 @@
 set :stage, :pontos
 server 'pontos.ucsd.edu', user: 'escowles', roles: %w{web app db}
 set :rails_env, "pontos"
+if ENV["CAP_SSHKEY_PONTOS"]
+  puts "Using key: #{File.join(ENV["HOME"], ENV["CAP_SSHKEY_PONTOS"])}"
+  set :ssh_options, { keys: File.join(ENV["HOME"], ENV["CAP_SSHKEY_PONTOS"]) }
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,7 @@
 set :stage, :production
 server 'lib-hydrahead.ucsd.edu', user: 'rvm', roles: %w{web app db}
 set :rails_env, "production"
+if ENV["CAP_SSHKEY_PRODUCTION"]
+  puts "Using key: #{File.join(ENV["HOME"], ENV["CAP_SSHKEY_PRODUCTION"])}"
+  set :ssh_options, { keys: File.join(ENV["HOME"], ENV["CAP_SSHKEY_PRODUCTION"]) }
+end
