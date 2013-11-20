f3fca9eac512e5579fc90479b1a1837f0be06460
david <david@naruto.local>
2013-11-20 15:41:40 -0800

    Correcting search pager display issue (DHH-248)

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -5,40 +5,48 @@ module CatalogHelper
   # Use the catalog_path RESTful route to create a link to the show page for a specific item.
   # catalog_path accepts a HashWithIndifferentAccess object. The solr query params are stored in the session,
   # so we only need the +counter+ param here. We also need to know if we are viewing to document as part of search results.
-  def link_to_document(doc, opts={:label=>nil, :counter => nil, :results_view => true})
-	opts[:label] ||= blacklight_config.index.show_link.to_sym
-    if doc['title_json_tesim'] != nil
-      titlehash = JSON.parse doc['title_json_tesim'].first
-      if titlehash['subtitle'] != nil
-        label = titlehash['value'].html_safe
-        label += ": #{titlehash['subtitle']}".html_safe if !titlehash['subtitle'].blank?
-      else
-        label = titlehash['value'].html_safe
+  def link_to_document(doc, opts={:label => nil, :counter => nil, :results_view => true, :force_label => false})
+
+    opts[:label] ||= blacklight_config.index.show_link.to_sym
+    label = render_document_index_label( doc, opts ).html_safe
+
+    if opts[:force_label] == false
+
+      if doc['title_json_tesim'] != nil
+        titlehash = JSON.parse doc['title_json_tesim'].first
+        if titlehash['subtitle'] != nil
+          label = titlehash['value'].html_safe
+          label += ": #{titlehash['subtitle']}".html_safe if !titlehash['subtitle'].blank?
+        else
+          label = titlehash['value'].html_safe
+        end
       end
-    else
-      label = render_document_index_label( doc, opts ).html_safe
+
     end
+
     if doc['type_tesim'] != nil && doc['type_tesim'].include?("Collection")
       url = dams_collection_path(doc, :counter => opts[:counter] )
     else
       url = dams_object_path(doc, :counter => opts[:counter] )
     end
-    link_to label, url, { :'data-counter' => opts[:counter] }.merge(opts.reject { |k,v| [:label, :counter, :results_view].include? k  })
-  end
-  
-  def should_render_index_field? document, solr_field
-  	if !(solr_field.is_a? Symbol) && !(solr_field.is_a? String)
-		hitsonly = solr_field.hitsonly
-	end
-	if (hitsonly)
-		return document.has_highlight_field? solr_field.field
-	else
-		return document.has?(solr_field.field) ||
-			(document.has_highlight_field? solr_field.field if solr_field.highlight)
-	end
+
+    link_to label, url, { :'data-counter' => opts[:counter] }.merge(opts.reject { |k,v| [:label, :counter, :force_label, :results_view].include? k  })
+
   end
 
-  
+  def should_render_index_field? document, solr_field
+    if !(solr_field.is_a? Symbol) && !(solr_field.is_a? String)
+      hitsonly = solr_field.hitsonly
+    end
+    if (hitsonly)
+      return document.has_highlight_field? solr_field.field
+    else
+      return document.has?(solr_field.field) ||
+          (document.has_highlight_field? solr_field.field if solr_field.highlight)
+    end
+  end
+
+
   #use blacklight add_facet_params to construct facet link
   #see sufia for reference if need to add more: https://github.com/psu-stewardship/sufia/blob/master/app/helpers/sufia_helper.rb
   # field = field value (i.e. Academic Dissertations)
@@ -50,7 +58,7 @@ module CatalogHelper
   def facet_uri(field, field_string)
     return add_facet_params(field_string, field).merge!({"controller" => "catalog", :action=> "index"})
   end
-  
+
   #override of blacklight helper function: 
   # https://github.com/projectblacklight/blacklight/blob/master/app/helpers/blacklight/blacklight_helper_behavior.rb#L353
   # uses a semi-colon delimiter in for search result field values
@@ -145,7 +153,7 @@ module CatalogHelper
         # NO PREVIEW IMAGE: choose format-appropriate glyphicon
         fieldData = document['files_tesim']
         simpleUse = nil
-  
+
         if fieldData != nil
           fieldData.each do |datum|
             files = JSON.parse(datum)
@@ -177,11 +185,11 @@ module CatalogHelper
           resultClass = 'thumb-simple'
         end
       end
- 
+
       resultClass = 'thumb-collection'
       resultIcon = 'glyphicon-th'
     end
- 
+
     [resultClass, resultIcon, resultThumb]
   end
 end
