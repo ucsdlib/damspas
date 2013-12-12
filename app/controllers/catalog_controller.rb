9c729ce4f3082e7acedfac33b2d3c1ffbd940044
lsitu <lsitu@ucsd.edu>
2013-12-12 14:02:03 -0800

    Adding the id for the default keyword search for now since it causes a fail test case in "retrieve a unit record" which uses an ark id for searching as discovered by Vivian. (DHH-201)

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -57,7 +57,7 @@ class CatalogController < ApplicationController
     config.default_solr_params = { 
       :qt => 'search',
       :rows => 20,
-      :qf => 'all_fields_tesim fulltext_tesim',
+      :qf => 'all_fields_tesim fulltext_tesim id',
     }
 	
 	#UCSD custom added argument config.highlighting to turn on/off hit highlighting with config.highlighting=true|false
@@ -162,7 +162,7 @@ class CatalogController < ApplicationController
     
     config.add_search_field ('Keyword (Title, Name/Creator, Topic, Notes etc.)') do |field|
 	  #field.solr_parameters = { :'qf' => 'all_fields_tesim' }
-	  field.solr_parameters = { :'qf' => 'all_fields_tesim fulltext_tesim' }
+	  field.solr_parameters = { :'qf' => 'all_fields_tesim fulltext_tesim id' }
 	end 
     config.add_search_field('Title') do |field|
       # solr_parameters hash are sent to Solr as ordinary url query params. 
