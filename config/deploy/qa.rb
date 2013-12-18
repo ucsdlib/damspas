23fdcf6247b472e9b3c0149c86d608bb4aa89555
Esmé Cowles <escowles@ucsd.edu>
2013-12-18 09:46:46 -0500

    Adding QA environment files

---
+++ config/deploy/qa.rb
@@ -0,0 +1,8 @@
+set :stage, :qa
+set :branch, 'qa'
+server 'lib-hydrahead-qa.ucsd.edu', user: 'rvm', roles: %w{web app db}
+set :rails_env, "qa"
+if ENV["CAP_SSHKEY_QA"]
+  puts "Using key: #{File.join(ENV["HOME"], ENV["CAP_SSHKEY_QA"])}"
+  set :ssh_options, { keys: File.join(ENV["HOME"], ENV["CAP_SSHKEY_QA"]) }
+end
