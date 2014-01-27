5a77bb1d52a70076eb97a0ebb324228333ef0233
lsitu <lsitu@ucsd.edu>
2014-01-27 12:41:08 -0800

    Fixed those existing test cases that failed for the gated access control matters. (DHH-251)

--- spec/controllers/mads_complex_subjects_controller_spec.rb
+++ spec/controllers/mads_complex_subjects_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe MadsComplexSubjectsController do
 	before do
-		sign_in User.create!
+		sign_in User.create! ({:provider => 'developer'})
   end
 
   describe "when signed in" do
