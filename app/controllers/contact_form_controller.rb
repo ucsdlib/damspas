2a614c3d1411e8226f79ac40227be6e7c3e8bfa1
Huawei Weng <wenghuawei@yahoo.com>
2014-02-19 16:32:26 -0800

    Created model,controller and view for contact_form. (DHH-330)

---
+++ app/controllers/contact_form_controller.rb
@@ -0,0 +1,35 @@
+class ContactFormController < ApplicationController
+  
+  def new
+    @contact_form = ContactForm.new
+  end
+
+  def create
+    @contact_form = ContactForm.new(params[:contact_form])
+    @contact_form.request = request
+    # not spam and a valid form
+    logger.warn "*** MARK ***"
+    if @contact_form.deliver
+      flash.now[:notice] = 'Thank you for your message!You message has been sent.'
+      after_deliver
+      render :new
+    else
+      flash[:error] = 'Sorry, this message was not sent successfully. ' 
+      flash[:error] << @contact_form.errors.full_messages.map { |s| s.to_s }.join(",")
+      render :new
+    end
+  rescue 
+      flash[:error] = 'Sorry, this message was not delivered.'
+      render :new
+  end
+
+  def after_deliver
+     return unless false
+  end
+  
+end
+
+
+
+
+ 
---
+++ app/models/contact_form.rb
@@ -0,0 +1,26 @@
+class ContactForm < MailForm::Base
+include MailForm::Delivery
+
+   ISSUE_TYPES = [ 
+      ["General inquiry or request", "General inquiry or request"],
+      ["Feedback", "Feedback"],
+      ["Reporting a problem", "Reporting a problem"],
+   ] 
+  attribute :contact_method,  :captcha  => true
+  attribute :category,    :validate => true
+  attribute :name,        :validate => true
+  attribute :email,       :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :subject,     :validate => true
+  attribute :message,     :validate => true
+  # - can't use this without ActiveRecord::Base validates_inclusion_of :issue_type, :in => ISSUE_TYPES
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "Contact Form:#{subject}",
+      :to => "wengNY@gmail.com", 
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
