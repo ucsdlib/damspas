6a5ee6daaa3391669bda5e9d83dd5e2689d7103d
Esme Cowles <escowles@ucsd.edu>
2014-02-20 06:10:53 -0800

    Switching sitemap to sitemap_generator gem, working object/collection listing from solr (DHH-346)

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,50 +1,55 @@
-require 'rsolr'
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://library.ucsd.edu/dc"
+SitemapGenerator::Sitemap.compress = :all_but_first
 
-Sitemap::Generator.instance.load( :host => "library.ucsd.edu/dc" ) do
+SitemapGenerator::Sitemap.create do
 
   # static pages
-  path :root, priority: 1, change_frequency: 'monthly'
-  path :about, change_frequency: 'monthly'
-  path :faq, change_frequency: 'monthly'
-  path :takedown, change_frequency: 'monthly'
+  add '/about'
+  add '/faq'
+  add '/takedown'
 
   # resources from solr
-  records( "DamsObject", :dams_object )
-  records( "DamsAssembledCollection", :dams_collection )
-  records( "DamsProvenanceCollection", :dams_collection )
-  records( "DamsProvenanceCollectionPart", :dams_collection )
-end
-
-
-def records( record_type, record_path )
-
-  rows = 100
-  done = 0
-  total = 0
-  more_records = true
-  solr = RSolr.connect( :url => ActiveFedora.solr_config[:url] )
-  while ( more_records )
-    # get a batch of records from solr
-    solr_response = solr.get 'select', :params => {:q => "has_model_ssim:\"info:fedora/afmodel:#{record_type}\" AND read_access_group_ssim:public", :rows => rows, :wt => :ruby, :start => done}
-    response = solr_response['response']
-    if done == 0
-      puts "#{record_type}: #{response['numFound']}"
-      total = response["numFound"]
-    end
-    done += rows
-
-    # output each record
-    @records = response['docs']
-    @records.each do |rec|
-      id = rec['id']
-      lastmod = rec['timestamp']
-      puts "  id: #{id}, lastmod: #{lastmod}"
-      path record_path, params: { id: id }, priority: 0.9, change_frequency: 'monthly', updated_at: lastmod
-    end
-
-    # stop looping if this is the last batch
-    if done >= total
-      more_records = false
+  resources = {
+    :DamsObject => :object,
+    :DamsAssembledCollection => :dams_collections,
+    :DamsProvenanceCollection => :dams_collections,
+    :DamsProvenanceCollectionPart => :dams_collections
+  }
+  resources.each do |record_type,record_path|
+    rows = 100
+    done = 0
+    total = 0
+    more_records = true
+    solr = RSolr.connect( :url => ActiveFedora.solr_config[:url] )
+    while ( more_records )
+      # get a batch of records from solr
+      solr_response = solr.get 'select', :params => {:q => "has_model_ssim:\"info:fedora/afmodel:#{record_type}\" AND read_access_group_ssim:public", :rows => rows, :wt => :ruby, :start => done}
+      response = solr_response['response']
+      if done == 0
+        if SitemapGenerator::Sitemap.verbose
+          puts "#{record_type}: #{response['numFound']} records"
+        end
+        total = response["numFound"]
+      end
+      done += rows
+  
+      # output each record
+      @records = response['docs']
+      @records.each do |rec|
+        id = rec['id']
+        lastmod = rec['timestamp']
+        if SitemapGenerator::Sitemap.verbose
+          puts "#{record_type}: #{id}, lastmod: #{lastmod}"
+        end
+        add "#{record_path}/#{id}", priority: 0.9, :changefreq => 'monthly', :lastmod => lastmod
+      end
+  
+      # stop looping if this is the last batch
+      if done >= total
+        more_records = false
+      end
     end
   end
+
 end
