a9ceb787ac60817bac3c7793e8811343ff16d175
lsitu <lsitu@ucsd.edu>
2014-03-13 15:13:12 -0700

    Logging the ip for anonymous and ucsd local access. (DCMKEY-361)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
       anon = User.anonymous(request.remote_ip)
       if anon.to_s != 'public'
         @current_user = anon
-        logger.warn "#{self.class.name}: wrapping request in anonymous user session (#{current_user}) to_s: #{anon.to_s}"
+        logger.warn "#{self.class.name}: wrapping request in anonymous user session (#{current_user})  from ip #{request.remote_ip} to_s: #{anon.to_s}"
+      else
+      	logger.info "#{self.class.name}: wrapping request in anonymous user session (#{current_user}) from ip #{request.remote_ip} to_s: #{anon.to_s}"
       end
       begin
         yield
