28fb4a912ce84bc6dd3761dc0e8970ef24cc09d3
david <david@naruto.local>
2014-03-17 12:56:47 -0700

    Updating home page script to use new collections path (DHH-391)

--- app/assets/javascripts/home-page.js
+++ app/assets/javascripts/home-page.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	var roll = Math.floor(Math.random()*bg_data.length);
 	var bg_img_url = "http://libraries.ucsd.edu/assets/dams/home-page/img/" + bg_data[roll].ark + ".jpg";
-	var collection_url = $("meta[name=dams_root]").attr("content") + "dams_collections/" + bg_data[roll].ark;
+	var collection_url = $("meta[name=dams_root]").attr("content") + "collection/" + bg_data[roll].ark;
 	var collection_title = (bg_data[roll].title != "") ? "<p>" + bg_data[roll].title : "";
 	var collection_creator = (bg_data[roll].creator != "") ? "<p>" + bg_data[roll].creator : "";
 	var collection_name = (bg_data[roll].collection != "") ? "<p>From the " + bg_data[roll].collection + " Collection" : "<p>View the collection" ;
