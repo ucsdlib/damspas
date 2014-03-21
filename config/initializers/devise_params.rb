603ad98ba24c945598bb22c1ca4774b531dba4e3
Huawei Weng <wenghuawei@yahoo.com>
2014-03-21 09:50:02 -0700

    Removed givenName from config.(DHH-357)

--- config/initializers/devise_params.rb
+++ config/initializers/devise_params.rb
@@ -1,7 +1,7 @@
 DeviseController.class_eval do
   def resource_params
     unless params[resource_name].blank?
-      params.require(resource_name).permit(:givenName, :name,:email, :provider, :uid, :groups, :anonymous)
+      params.require(resource_name).permit(:name,:email, :provider, :uid, :groups, :anonymous)
     end
   end
 end
