abb54bfa2d2c2f9d6fec6f6285a84f24694de05e
Esmé Cowles <escowles@ucsd.edu>
2014-06-19 17:44:44 -0400

    Changing username for deploys to conan (the deployer)

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,6 +1,6 @@
 set :stage, :qa
 set :branch, 'feature/rbenv'
-server 'lib-hydrahead-qa.ucsd.edu', user: 'rvm', roles: %w{web app db sitemap_noping}
+server 'lib-hydrahead-qa.ucsd.edu', user: 'conan', roles: %w{web app db sitemap_noping}
 set :rails_env, "qa"
 if ENV["CAP_SSHKEY_QA"]
   puts "Using key: #{File.join(ENV["HOME"], ENV["CAP_SSHKEY_QA"])}"
