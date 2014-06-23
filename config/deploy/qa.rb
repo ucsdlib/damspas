c50ac0550ed6d21da54096133ce2dd0b0e682e5d
Esmé Cowles <escowles@ucsd.edu>
2014-06-23 18:56:35 -0400

    Updating deploy branch/user settings

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,5 +1,5 @@
 set :stage, :qa
-set :branch, 'feature/rbenv'
+set :branch, 'develop'
 server 'lib-hydrahead-qa.ucsd.edu', user: 'conan', roles: %w{web app db sitemap_noping}
 set :rails_env, "qa"
 if ENV["CAP_SSHKEY_QA"]
