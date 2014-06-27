af70613c62cf13bacff183a4690e84dd014dfa00
Esmé Cowles <escowles@ucsd.edu>
2014-06-27 17:11:51 -0400

    Switching back to develop branch

--- config/deploy/pontos.rb
+++ config/deploy/pontos.rb
@@ -1,5 +1,5 @@
 set :stage, :pontos
-set :branch, 'feature/search-within-results'
+set :branch, 'develop'
 server 'pontos.ucsd.edu', user: 'rvm', roles: %w{app db}
 set :rails_env, "pontos"
 if ENV["CAP_SSHKEY_PONTOS"]
