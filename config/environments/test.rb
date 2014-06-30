865b91bbbbd9a518cb2144edb24ca3e9c0319c47
Huawei Weng <wenghuawei@yahoo.com>
2014-06-30 15:59:58 -0700

    Fixing the unexpected changes from rake rails:update.(DHH-490)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,5 @@
-Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+Hydra::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
@@ -7,33 +7,52 @@ Rails.application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
-  # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
+  # Configure static asset server for tests with Cache-Control for performance
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=3600"
+
   config.eager_load = false
 
-  # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_assets  = true
-  config.static_cache_control = 'public, max-age=3600'
-
-  # Show full error reports and disable caching.
+  # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Raise exceptions instead of rendering exception templates.
+  # Raise exceptions instead of rendering exception templates
   config.action_dispatch.show_exceptions = false
 
-  # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
+  # Disable request forgery protection in test environment
+  config.action_controller.allow_forgery_protection    = false
 
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+   # action mailer 
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'dlp@ucsd.edu'}
 
-  # Print deprecation notices to the stderr.
+  # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  # i18n
+  config.i18n.fallbacks = true
+
+  # environment-specific DAMS config
+  # ip-based role assignment
+  config.public_ips = []
+  config.local_ip_blocks = [ "192.168.0.", "192.168.1." ]
+  config.wowza_directory = '/pub/data2/dams/'
+  config.wowza_baseurl = 'lib-streaming.ucsd.edu:1935/dams4-test/_definst_/'
+  config.role_vocab = "#{config.id_namespace}bb14141414"
+  config.lang_vocab ="#{config.id_namespace}bb43434343"
+  config.excluded_collections = "(bd5905379f OR bb13664503)"
+  config.developer_groups = ['developer-authenticated','dams-curator','dams-manager-admin', 'dams-manager-user']
+  config.curator_groups = ['dams-curator','dams-rci','dams-manager-admin']
+  config.super_role = 'dams-manager-admin'
+  config.unknown_groups = ['unknown']
+  config.zoomify_baseurl = 'http://rohan.ucsd.edu/zoomify/'
+  config.shibboleth = false
 end
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,18 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+Mime::Type.register_alias "text/html", :textile
+Mime::Type.register_alias "text/html", :inline
+
+Mime::Type.register_alias "text/plain", :refworks_marc_txt
+Mime::Type.register_alias "text/plain", :openurl_kev
+Mime::Type.register "application/x-endnote-refer", :endnote
+Mime::Type.register "application/marc", :marc
+Mime::Type.register "application/marcxml+xml", :marcxml, 
+      ["application/x-marc+xml", "application/x-marcxml+xml", 
+       "application/marc+xml"]
+Mime::Type.register "application/rdf+xml", :rdf
+
+
+
