bd2992d30dc42c6a5d67ce7455f9674ada5e4755
VivianChu <tchu@ucsd.edu>
2014-07-11 15:24:41 -0700

    Changing config files for gimili and pontos.

--- config/deploy/gimili.rb
+++ config/deploy/gimili.rb
@@ -1,6 +1,6 @@
 set :stage, :gimili
 set :branch, 'develop'
-server 'gimili.ucsd.edu', user: 'escowles', roles: %w{app db sitemap_noping}
+server 'gimili.ucsd.edu', user: 'conan', roles: %w{app db sitemap_noping}
 set :rails_env, "gimili"
 if ENV["CAP_SSHKEY_GIMILI"]
   puts "Using key: #{File.join(ENV["HOME"], ENV["CAP_SSHKEY_GIMILI"])}"
