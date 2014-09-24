9fbec564df73ed2a4b14969bff5aa5a97fbe9924
Huawei Weng <hweng@ucsd.edu>
2014-09-24 12:49:10 -0700

    Adding integration test. (DHH-537)

--- spec/features/advanced_spec.rb
+++ spec/features/advanced_spec.rb
@@ -6,3 +6,12 @@ describe "keyword field" do
       expect(page).to have_selector("input[value='fish']")
   end
 end
+
+describe "facet field" do
+  it "should prefilled the facet if the facet params exist" do
+      visit '/advanced?f[unit_sim][]=Library+Digital+Collections&f[object_type_sim][]=image'
+      expect(page).to have_selector("input[id='f_inclusive_object_type_sim_image'][checked='checked']")
+      expect(page).to have_selector("input[id='f_inclusive_unit_sim_Library_Digital_Collections'][checked='checked']")
+  end
+end
+
