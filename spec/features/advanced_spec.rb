1686720022e2423ac41bd9b0c0487230e4a4c08e
Huawei Weng <hweng@ucsd.edu>
2014-09-26 10:22:41 -0700

    Tweak the test.(DHH-537)

--- spec/features/advanced_spec.rb
+++ spec/features/advanced_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "keyword field" do
-  it "should prefilled the search term if the params exist" do
+  it "should prefill the search term if the params exist" do
       visit '/advanced?q=fish'
       expect(page).to have_selector("input[value='fish']")
   end
