ca2055681a9a96d3cc5fad92b94726b15560d235
Huawei Weng <hweng@ucsd.edu>
2014-10-06 15:25:39 -0700

    Updated controller tests for all related classes.(DHH-545)

--- spec/controllers/dams_provenance_collection_parts_controller_spec.rb
+++ spec/controllers/dams_provenance_collection_parts_controller_spec.rb
@@ -21,54 +21,6 @@ describe DamsProvenanceCollectionPartsController do
           @newobj.resource_type.should == @obj.resource_type
 	    end
 	  end
-	  
-	  describe "New" do
-	    it "should be successful" do 
-	      get :new
-	      response.should be_successful 
-	      assigns[:dams_provenance_collection_part].should be_kind_of DamsProvenanceCollectionPart
-	    end
-	  end
-	  
-	  describe "Edit" do
-	    before do
-	      @obj = DamsProvenanceCollectionPart.create(titleValue: "Test Provenance Collection Title", beginDate: "2012", endDate: "2013", visibility: "public", resource_type: "text")
-          solr_index @obj.pid
-	    end    
-	    it "should be successful" do 
-	      get :edit, id: @obj.id
-	      response.should be_successful 
-	      @newobj = assigns[:dams_provenance_collection_part]
-          @newobj.titleValue.should == @obj.titleValue
-          @newobj.beginDate.should == @obj.beginDate
-          @newobj.endDate.should == @obj.endDate
-          @newobj.visibility.should == @obj.visibility
-          @newobj.resource_type.should == @obj.resource_type
-	    end
-	  end
-	  
-	  describe "Create" do
-	    it "should be successful" do
-	      expect { 
-	        post :create, :dams_provenance_collection_part => {titleValue: ["Test Title"], beginDate: ["2013"]}
-        }.to change { DamsProvenanceCollectionPart.count }.by(1)
-	      response.should redirect_to assigns[:dams_provenance_collection_part]
-	      assigns[:dams_provenance_collection_part].should be_kind_of DamsProvenanceCollectionPart
-	    end
-	  end
-	  
-	  describe "Update" do
-	    before do
- 	      @obj = DamsProvenanceCollectionPart.create(titleValue: "Test Provenance Collection Title", beginDate: "2012", endDate: "2013")
-          solr_index @obj.pid
- 	    end
-	    it "should be successful" do
-	      put :update, :id => @obj.id, :dams_provenance_collection_part => {titleValue: ["Test Title2"], beginDate: ["2013"]}
-	      response.should redirect_to assigns[:dams_provenance_collection_part]
-	      @obj.reload.titleValue.should == "Test Provenance Collection Title"
-	      flash[:notice].should == "Successfully updated provenance_collection_part"
-	    end
-    end
   end
 end
 
--- spec/controllers/mads_personal_names_controller_spec.rb
+++ spec/controllers/mads_personal_names_controller_spec.rb
@@ -19,49 +19,6 @@ describe MadsPersonalNamesController do
           @newobj.externalAuthority.should == @obj.externalAuthority
 	    end
 	  end
-	  
-	  describe "New" do
-	    it "should be successful" do 
-	      get :new
-	      response.should be_successful 
-	      assigns[:mads_personal_name].should be_kind_of MadsPersonalName
-	    end
-	  end
-	  
-	  describe "Edit" do
-	    before do
-	      @obj = MadsPersonalName.create(name: "Personal Name", externalAuthority:  "http://lccn.loc.gov/n90694888")
-	    end    
-	    it "should be successful" do 
-	      get :edit, id: @obj.id
-	      response.should be_successful 
-	      @newobj = assigns[:mads_personal_name]
-          @newobj.name.should == @obj.name
-          @newobj.externalAuthority.should == @obj.externalAuthority
-	    end
-	  end
-	  
-	  describe "Create" do
-	    it "should be successful" do
-	      expect { 
-	        post :create, :mads_personal_name => {name: ["Test Name"] , externalAuthority:  "http://lccn.loc.gov/n90694888"}
-        }.to change { MadsPersonalName.count }.by(1)
-	      response.should redirect_to assigns[:mads_personal_name]
-	      assigns[:mads_personal_name].should be_kind_of MadsPersonalName
-	    end
-	  end
-	  
-	  describe "Update" do
-	    before do
- 	      @obj = MadsPersonalName.create(name: "Personal Name", externalAuthority:  "http://lccn.loc.gov/n90694888")
- 	    end
-	    it "should be successful" do
-	      put :update, :id => @obj.id, :mads_personal_name => {name: ["Test Title2"], externalAuthority:  ["http://lccn.loc.gov/n90694888"]}
-	      response.should redirect_to assigns[:mads_personal_name]
-	      @obj.reload.name.should == ["Test Title2"]
-	      flash[:notice].should == "Successfully updated personal_name"
-	    end
-    end
   end
 end
 
