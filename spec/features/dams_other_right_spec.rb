af3b85dc27337fb48d1408b187a85e9d07713afb
Huawei Weng <hweng@ucsd.edu>
2014-10-06 15:46:55 -0700

    Updated feature tests for all related forms. (DHH-545)

--- spec/features/dams_other_right_spec.rb
+++ spec/features/dams_other_right_spec.rb
@@ -9,34 +9,6 @@ class Path
 	@path = nil
 end
 
-feature 'Visitor wants to create/edit a DAMS OtherRight' do
-  let!(:role) { MadsAuthority.create!(name: 'Authority Role Test') }
-  let!(:name) { MadsName.create!(name: 'Test Name') }
-
-  after do
-    role.destroy
-    name.destroy
-  end
-	scenario 'is on new DAMS OtherRight page' do
-		sign_in_developer
-		visit "dams_other_rights"
-		expect(page).to have_selector('a', :text => "Create OtherRights")
-		
-		visit dams_other_right_path('new')
-		# Create new OtherRight
-		fill_in "Note", :with => "Test Note"
-		fill_in "URI", :with => "http://b.com"
-		fill_in "Permission Type", :with => "display"
-		fill_in "Permission Begin Date", :with => "2011"
-		fill_in "Permission End Date", :with => "2012"
-		fill_in "Restriction Type", :with => "restrict"
-		fill_in "Restriction Begin Date", :with => "2013"
-		fill_in "Restriction End Date", :with => "2014"
-		#page.select('Authority Role Test', match: :first) 
-		#page.select('Test Name', match: :first) 
-		click_on "Save"	
-	end
-end
 def sign_in_developer
   visit new_user_session_path
   fill_in "name", :with => "name"
