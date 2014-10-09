ad8891cd0da8e37b5e504a7618f6ac2ad693ec5a
VivianChu <tchu@ucsd.edu>
2014-10-09 13:43:14 -0700

    Update the test to  disable the audio derivative download. DHH-532

--- spec/features/file_spec.rb
+++ spec/features/file_spec.rb
@@ -82,5 +82,11 @@ feature "Derivative download" do
     visit catalog_index_path( {:q => 'sample'} )
     click_link "Sample Image Component"
     expect(page).to have_link('Download file', href:"/object/bd3379993m/_2.jpg/download")
-  end 
+  end
+  scenario 'is on the view page for single audio object and the download link is disabled' do
+    visit catalog_index_path( {:q => 'sample'} )
+    click_link "The Sample Audio Object: I need another green form"
+    #expect(page).not_to have_link('Download file', href:"/object/bd5939745h/_2.mp3/download")
+    expect(page).not_to have_link('Download file', href:"/object/bd5939745h/_2.mp3/download")
+  end  
 end
