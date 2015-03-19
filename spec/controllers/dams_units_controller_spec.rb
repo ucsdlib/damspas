9db6e7aec6ea32acd038a0bb495b30c4cccbb2ea
VivianChu <tchu@ucsd.edu>
2015-03-19 15:21:48 -0700

    Updating RDCP Repository link. DHH-642

--- spec/controllers/dams_units_controller_spec.rb
+++ spec/controllers/dams_units_controller_spec.rb
@@ -5,14 +5,14 @@ describe DamsUnitsController do
   describe "GET 'view'" do
     before do
       sign_in User.create! ({:provider => 'developer'})
-      @unit = DamsUnit.create pid: "un48484848", name: "Research Data Curation Program", description: "Research Cyberinfrastructure: the hardware, software, and people that support scientific research.", uri: "http://rci.ucsd.edu/", code: "unrci", group: "dams-rci"
+      @unit = DamsUnit.create pid: "un48484848", name: "Research Data Curation Program", description: "Research Cyberinfrastructure: the hardware, software, and people that support scientific research.", uri: "http://rci.ucsd.edu/", code: "unrdcp", group: "dams-rci"
     end
     after do
       @unit.delete
     end
 
     it "returns http success" do
-      get 'show', :id => 'unrci'
+      get 'show', :id => 'unrdcp'
       response.should be_success
     end
   end
