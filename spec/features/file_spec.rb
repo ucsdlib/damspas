adcd3829fc2d7efa3d5a04f8cf36651fd992d22c
VivianChu <tchu@ucsd.edu>
2015-04-22 10:41:07 -0700

    Fixing the download issue for CEO content in curator mode. DHH-654

--- spec/features/file_spec.rb
+++ spec/features/file_spec.rb
@@ -93,3 +93,20 @@ describe "Download more than one master file" do
     expect(page).to have_link('', href:"/object/xx21171293/_2.tgz/download")    
   end
 end
+
+describe "to download file in a CEO complex object" do
+  before do
+    @ceoComplexObj = DamsObject.new(pid: "zz57691289")
+  end
+  after do
+    @ceoComplexObj.delete
+  end
+  it "should see a download button for the component file" do
+    @ceoComplexObj.damsMetadata.content = File.new('spec/fixtures/damsComplexObjectCeo.rdf.xml').read
+    @ceoComplexObj.save!
+    solr_index (@ceoComplexObj.pid)
+    sign_in_developer 
+    visit dams_object_path(@ceoComplexObj.pid)
+    expect(page).to have_link('', href:"/object/zz57691289/_1_2.jpg/download")  
+  end
+end
