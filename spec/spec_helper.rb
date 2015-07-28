576cefdc00b3b1d8735b568836a3065c0ca3fb52
Huawei Weng <hweng@ucsd.edu>
2015-07-28 12:09:48 -0700

    Updating codeclimate config.(DHH-717)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
-# require 'simplecov'
+
 require 'rspec/matchers'
 require 'equivalent-xml'
-# SimpleCov.start 'rails'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
