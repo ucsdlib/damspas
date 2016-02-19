d49185ad3160f66403ca32061a87d9909dda3e8e
Huawei Weng <hweng@ucsd.edu>
2016-02-18 16:44:01 -0800

    Added SHARE API mapping.(DAMSKEY-521)

--- lib/dams/controller_helper.rb
+++ lib/dams/controller_helper.rb
@@ -1,6 +1,155 @@
 module Dams
   module ControllerHelper
 
+		#Mapping for OSF API
+		def osf_title(document)
+			  field_name = "title_json_tesim"
+		    dams_data= document["#{field_name}"]
+		    osf_data=''
+
+		  if dams_data != nil
+		    dams_data.each do |datum|
+		      title = JSON.parse(datum)
+		      osf_data = title['name'] ? title['name'] : ''
+		      osf_data += title['name'] && title['translationVariant'] ? ' : ' : ''
+		      title_trans = title['translationVariant'] || []
+		      if title_trans.class == Array
+		      	title_trans.each do |trans|  
+		      		osf_data +=	trans
+		      	end
+		      elsif title_trans.class == String
+		      	osf_data +=	title_trans
+		      end
+		    end
+		  end
+		  osf_data
+		end
+
+		def osf_contributors(document)
+			field_name = "relationship_json_tesim"
+			dams_data = document["#{field_name}"]
+			osf_data =[]
+
+			if dams_data != nil
+		    dams_data.each do |datum|
+		    	
+		      relationships = JSON.parse(datum)
+		    	relationships.each do |key, value|
+		    		value.each do |v|
+		   				osf_data << {"name": v}
+		   			end
+		   		end
+		    end
+		  end
+		  osf_data
+		end
+
+		def osf_description(document)
+			field_name = "otherNote_json_tesim"
+			dams_data = document["#{field_name}"]
+			osf_data = ''
+
+			if dams_data != nil
+		    dams_data.each do |datum|
+		      other_note = JSON.parse(datum)
+		      osf_data = other_note['value'] if other_note['type'] == 'description'
+		    end
+		  end
+		  osf_data
+		end 
+
+		def osf_uris(document)
+			field_name = "id"
+			dams_data = document["#{field_name}"]
+			osf_data = {}
+
+			if dams_data != nil
+				url = "http://library.ucsd.edu/dc/collection/#{dams_data}"
+		    osf_data = {"canonicalUri": url, "providerUris": url}
+		  end
+		  osf_data
+		end
+
+		def osf_date(document)
+			field_name = "date_json_tesim"
+			dams_data = document["#{field_name}"]
+			osf_data = ''
+			
+			if dams_data != nil
+		    dams_data.each do |datum|
+		      date = JSON.parse(datum)
+		      osf_data = Time.now
+		      osf_data = date['beginDate'] if date['type'] == 'issued'
+		    end
+		  end
+		  osf_data
+		end
+
+		def osf_languages(document)
+			field_name = "language_tesim"
+			dams_data = document["#{field_name}"]
+      langs = dams_data || []
+      osf_data = ''
+
+      if langs.class == Array
+      	langs.each do |lang|  
+      		osf_data +=	lang
+      	end
+      elsif langs.class == String
+      	osf_data +=	langs
+      end
+			osf_data 
+		end
+
+		def osf_mads_fields(document)
+			osf_data = []
+
+			field_names = [
+				'geographic_tesim', 
+				'topic_tesim',
+				'commonName_tesim', 
+				'scientificName_tesim', 
+				'corporateName_tesim',
+				'personalName_tesim',
+				'subject_tesim',
+				'genreForm_tesim',
+				'anatomy_tesim',
+				'cruise_tesim',
+				'series_tesim',
+				'culturalContext_tesim',
+				'lithology_tesim'
+			]
+		  field_names.each do |field_name|
+		    dams_data = document["#{field_name}"]
+			  if dams_data.kind_of?(String)
+			  	osf_data << dams_data 
+		    elsif dams_data.kind_of?(Array)
+		    	dams_data.each do |datum| 
+		    		osf_data << datum
+		    	end
+		    end 
+		  end
+		  osf_data
+		end
+
+		def osf_publisher
+			osf_data = { "name": "UC San Diego Library, Digital Collections", "uri": "http://library.ucsd.edu/dc"}
+		end
+
+		def export_to_API(document)
+		  field_map = {
+		    'title': osf_title(document),
+		    'description': osf_description(document),
+		    'contributor': osf_contributors(document),
+		    'uris': osf_uris(document),
+		    'languages': osf_languages(document),
+		    'providerUpdatedDateTime': osf_date(document),
+		    'tags': osf_mads_fields(document),
+		    'publisher': osf_publisher
+		  }
+		  json_data = {"jsonData": field_map}
+		end
+
 # Retrieve label from solr index instead of external record from repo
  def get_linked_object_label(id)
 	  	@doc = get_search_results(:q => "id:#{id}")
