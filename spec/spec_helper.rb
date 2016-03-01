f28d5d08126c2f75c4f6e16f048a8b19c33d5796
Huawei Weng <hweng@ucsd.edu>
2016-03-01 12:21:36 -0800

    Integrated ShareNotify and added share link.(DHH-788)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
 require 'rspec/matchers'
-require 'equivalent-xml'
+#require 'equivalent-xml'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
