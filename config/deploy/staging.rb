5d273056a0a9b99de6fb71e0d55b767a11668fa6
VivianChu <tchu@ucsd.edu>
2016-06-02 10:39:32 -0700

    Add sitemap role to staging deploy config. DHH-833

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
 set :branch, 'staging'
-server 'lib-hydrahead-staging.ucsd.edu', user: 'conan', roles: %w{web app db}
+server 'lib-hydrahead-staging.ucsd.edu', user: 'conan', roles: %w{web app db sitemap_noping}
 set :rails_env, "staging"
 set :ssh_options, {keepalive: true, keepalive_interval: 120 }
 if ENV["CAP_SSHKEY_STAGING"]
