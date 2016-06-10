407a46069dccce9d6159c3eeb89ae0d0d6f137d2
VivianChu <tchu@ucsd.edu>
2016-06-10 11:40:36 -0700

    Update staging deploy config. DHH-833

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,7 @@
 set :stage, :staging
 set :branch, 'feature/fix_deploy'
-server 'lib-hydrahead-staging.ucsd.edu', user: 'conan', roles: %w{web app db}
+server 'lib-hydrahead-staging.ucsd.edu', user: 'conan', roles: %w{web app db sitemap_noping}
 set :rails_env, "staging"
-set :ssh_options, {keepalive: true, keepalive_interval: 120 }
 if ENV["CAP_SSHKEY_STAGING"]
   puts "Using key: #{File.join(ENV["HOME"], ENV["CAP_SSHKEY_STAGING"])}"
   set :ssh_options, { keys: File.join(ENV["HOME"], ENV["CAP_SSHKEY_STAGING"]) }
