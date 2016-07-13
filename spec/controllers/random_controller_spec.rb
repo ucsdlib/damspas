0fdfea022afd8ed674611539f43f984921974022
VivianChu <tchu@ucsd.edu>
2016-07-13 08:40:22 -0700

    Fix random function issue. DHH-847
    Update random controller test. DHH-847
    
    Update qa deploy config.

--- spec/controllers/random_controller_spec.rb
+++ spec/controllers/random_controller_spec.rb
@@ -8,38 +8,22 @@ describe RandomController do
                subtitle: 'Test Subtitle', titleNonSort: 'The',
                titlePartName: 'Test Part', titlePartNumber: 'Test Number',
                titleTranslationVariant: 'Test Translation Variant',
-               copyright_attributes: [{status: 'Public domain'}]
-    @other_o = DamsObject.create titleValue: 'Test Title 2',
-               subtitle: 'Test Subtitle', titleNonSort: 'The',
-               titlePartName: 'Test Part', titlePartNumber: 'Test Number',
-               titleTranslationVariant: 'Test Translation Variant',
-               copyright_attributes: [{status: 'Public domain'}]
-    @acol = DamsAssembledCollection.create( titleValue: 'Test Assembled Collection',
-                  visibility: 'public' )
-    @pcol = DamsProvenanceCollection.create( titleValue: 'Test Provenance Collection',
-                  visibility: 'public' )                              
+               copyright_attributes: [{status: 'Public domain'}]                             
     solr_index @o.pid
-    solr_index @other_o.pid
-    solr_index @acol.pid
-    solr_index @pcol.pid
   end
   after(:all) do
-    @o.delete
-    @other_o.delete
-    @acol.delete
-    @pcol.delete      
+    @o.delete     
   end
 
   describe 'GET index' do
     before do
-      get :show, {:id => "item"}      
+      get :index, {:id => "item"}      
       @items = "#{response.body}"
     end  
     it 'get the random url' do
       get :index
       expect(response.status).to eq( 200 )
       expect(response.header['Content-Type']).to match /json/
-      
       random_obj_id = response.body.to_s[/\w{10}$/]
       expect(@items).to include(random_obj_id)
     end
