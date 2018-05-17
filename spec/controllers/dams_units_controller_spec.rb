0b32e557ccd88851f98b0e9068ae2162a2952cf8
lsitu <lsitu@ucsd.edu>
2018-05-16 17:01:33 -0700

    Fixes #446 - Add integration test to replicate the object counts issue and fix the problem.

--- spec/controllers/dams_units_controller_spec.rb
+++ spec/controllers/dams_units_controller_spec.rb
@@ -17,4 +17,34 @@ describe DamsUnitsController do
     end
   end
 
+  describe 'object counts' do
+    let(:unit) { DamsUnit.create name: 'Test Unit', description: 'Test Description', code: 'tu', uri: 'http://example.com/' }
+    let(:copyPublic) { DamsCopyright.create status: 'Public domain' }
+    let(:copyCurator) { DamsCopyright.create status: 'Under copyright' }
+    let(:objPublic) { DamsObject.create titleValue: 'Test Public Record', beginDate: '2013', unitURI: unit.pid, copyrightURI: copyPublic.pid }
+    let(:objCurator) { DamsObject.create titleValue: 'Test Private Record', unitURI: unit.pid, copyrightURI: copyCurator.pid }
+    let(:objects_count_response) { controller.instance_variable_get('@count_resp') }
+
+    before do
+      solr_index objPublic.pid
+      solr_index objCurator.pid
+    end
+
+    after do
+      [objPublic, objCurator, copyPublic, copyCurator, unit].each(&:delete)
+    end
+
+    it 'has the count public user' do
+      get 'index'
+
+      expect(objects_count_response.response['numFound']).to eq 1
+    end
+
+    it 'has the count for curator' do
+      sign_in User.create!(provider: 'developer')
+      get 'index'
+
+      expect(objects_count_response.response['numFound']).to eq 2
+    end
+  end
 end
