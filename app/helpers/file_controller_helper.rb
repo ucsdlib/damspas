f689543bca53b040467c8617c5383b0927059e24
Matt Critchlow <matt.critchlow@gmail.com>
2018-06-15 10:47:12 -0700

    Remove sendfile prefix from configuration
    This is now handled by leveraging the source path and filename values in
    files_tesim. The assumption here is that the nginx symlink will be setup
    such that localStore/<path-to-file> will be handled on the nginx mapping
    side of things.
    
    Also moved private file controller methods to a helper

---
+++ app/helpers/file_controller_helper.rb
@@ -0,0 +1,25 @@
+# Helper methods for the File controller
+module FileControllerHelper
+  # Construct a relative filepath for send_file's use of nginx
+  # An expected path would be like the following:
+  # Given source_path is /pub/data2/dams/localStore/bb/37/89/62/8p
+  # Given source_filename is 20775-bb3789628p-0-4.jpg
+  # Then return value is: localStore/20775-bb3789628p-0-4.jpg
+  def localstore_filename(source_path, source_filename)
+    source_path.split('dams/')[1] + '/' + source_filename
+  end
+  # Determine http content type for a file download
+  # We have added our own localized logic when a mimeType doesn't exist, such as checking the filename suffix for xml
+  # files. This ideally should not be necessary.
+  def http_content_type(datastream, filename)
+    return 'application/octet-stream' unless datastream && filename
+
+    if datastream.mimeType
+      datastream.mimeType
+    elsif filename.include?('.xml')
+      'application/xml'
+    else
+      'application/octet-stream'
+    end
+  end
+end
