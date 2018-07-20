21ae16a14063c52319ac25c967a244674f6977b8
Matthew Critchlow <matt.critchlow@gmail.com>
2018-07-20 12:26:11 -0700

    Mark ruby version spec as pending
    CircleCI image that must be used for the machine executor has an out of date ruby, but it is a 2.3 image, so the specs pass. Agreed to temporarily mark pending

--- spec/features/ruby_version_spec.rb
+++ spec/features/ruby_version_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 feature "ruby version" do
   scenario "objects should have meta tags" do
+    pending("Waiting on circleci image to support current ruby")
     ver = File.read(File.join(File.dirname(__FILE__), '..', '..', '.ruby-version')).chomp
     visit ruby_version_path
     expect(page).to have_text "ruby version = #{ver}"
