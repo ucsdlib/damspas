bb1b76d413b4bb834b96515b342816415ea9dc40
Kiah <kiah@notch8.com>
2018-10-10 18:25:21 -0700

    drastically simplify the rabbit hole that was db auth

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,6 +1,6 @@
 class PasswordsController < Devise::PasswordsController
   protected
   def after_sending_reset_password_instructions_path_for(resource_name)
-    new_user_email_session_path
+    new_user_session_path(email: true) if is_navigational_format?
   end
 end
