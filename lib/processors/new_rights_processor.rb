98cefb5b2fdc74aebdee93b623158f5c42361962
Kiah <kiah@notch8.com>
2018-10-23 12:10:45 -0700

    fix bug in rake and DRY up rspec

--- lib/processors/new_rights_processor.rb
+++ lib/processors/new_rights_processor.rb
@@ -8,19 +8,19 @@ module Processors
       puts 'begin task'
       email = @request_attributes[:email]
       if email
-        user = User.where(email: email).first_or_initialize do |user| # only hits the do on initialize
-          user.provider = 'auth_link',
+        @user = User.where(email: email).first_or_initialize do |user| # only hits the do on initialize
+          user.provider = 'auth_link'
           user.uid = SecureRandom::uuid
-          user.provider = JSON.parse(user.provider)[0] # Line 12 sets user.provider to a hash containing the values of both provider and the uid for a currently unknown reason
           user.ensure_authentication_token
+          puts 'new user created'
         end
-        if user.valid?
-          user.save!
-          AuthMailer.send_link(user).deliver_later
+        if @user.valid?
+          @user.save!
+          AuthMailer.send_link(@user).deliver_later
           puts "email sent!"
         end
       end
-      if user && user.valid?
+      if @user && @user.valid?
         puts ">>>>>> Task succeeded!"
       else
         puts ">>>>>> TASK FAILED"
--- spec/lib/processors/new_rights_processor_spec.rb
+++ spec/lib/processors/new_rights_processor_spec.rb
@@ -1,28 +1,67 @@
 require 'spec_helper'
 
 describe Processors::NewRightsProcessor do
-  let!(:response) { {email: 'test@example.com'} }
-  let!(:bad_response) { {email: 'invalid_email_format'} }
-  let!(:obj) { Processors::NewRightsProcessor.new(response) }
-  let!(:bad_obj) { Processors::NewRightsProcessor.new(bad_response) }
+  let!(:option){ }
+  let!(:response) { [{email: 'test@example.com'}, {email: 'invalid_email_format'}, {email: ''}] }
+  let!(:obj) { Processors::NewRightsProcessor.new(response[option]) }
 
   describe "initialize" do
-    it "if received valid request_attributes" do
-      expect(obj).to be_instance_of(Processors::NewRightsProcessor)
+    context "if rake task is run with valid credentials" do
+      let!(:option){ 0 }
+      it "runs successfully" do
+        expect(obj).to be_instance_of(Processors::NewRightsProcessor)
+      end
+    end
+
+    context "if rake task is run with invalid credentials" do
+      let!(:option){ 1 }
+      it "fails quietly" do
+        expect { obj.process }.to_not raise_error
+      end
     end
   end
 
   describe "process" do
-    it "runs successfully with valid attributes" do
-      expect { obj.process }.to_not raise_error
+    context "when credentials are valid" do
+      let!(:option){ 0 }
+      it "runs successfully" do
+        expect { obj.process }.to_not raise_error
+      end
+
+      it "sets user attributes correctly" do
+        obj.process # @user is not set in module until process method is run
+        user = obj.instance_variable_get(:@user)
+
+        expect(user.provider).to eq('auth_link')
+        expect(user.uid).to_not be_empty
+        expect(user.authentication_token).to_not be_empty
+      end
+
+      it "sends email to user on success" do
+        expect { obj.process }.to change { ActionMailer::Base.deliveries.count }.by(1)
+      end
     end
 
-    it "fails with invalid attributes" do
-      expect { bad_obj.process }.to change { User.count }.by(0)
+    context "when email has an invalid format" do
+      let!(:option){ 1 }
+      it "fails with invalid attributes" do
+        expect { obj.process }.to change { User.count }.by(0)
+      end
+
+      it "does not send email on failure" do
+        expect { obj.process }.to change { ActionMailer::Base.deliveries.count }.by(0)
+      end
     end
 
-    it "sends email to user on success" do
-      expect { obj.process }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    context "when email is blank" do
+      let!(:option){ 2 }
+      it "validates the presence of an email" do
+        expect { obj.process }.to change { User.count }.by(0)
+      end
+
+      it "does not send an email" do
+        expect { obj.process }.to change { ActionMailer::Base.deliveries.count }.by(0)
+      end
     end
   end
 end
