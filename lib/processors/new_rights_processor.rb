f8c9803ea082bbe21c23204a484b7f61bf207c89
Kiah <kiah@notch8.com>
2018-10-30 16:22:54 -0700

    more refactoring

--- lib/processors/new_rights_processor.rb
+++ lib/processors/new_rights_processor.rb
@@ -2,36 +2,43 @@ module Processors
   class NewRightsProcessor
     def initialize(request_attributes)
       @request_attributes = request_attributes
+      @work_pid = @request_attributes[:work_pid]
     end
 
     def process
       puts 'begin task'
-      email = @request_attributes[:email]
-      work_pid = @request_attributes[:work_pid]
-      work_obj = DamsObject.where(pid: work_pid).first
-      return unless email.present? && work_pid.present? && work_obj
-      @user = User.where(email: email).first_or_initialize do |user| # only hits the do on initialize
-        user.provider = 'auth_link'
-        user.uid = SecureRandom::uuid
-        user.ensure_authentication_token
-        puts 'new user created'
+      return unless @request_attributes[:email].present? && user.valid? && @work_pid.present? && work_obj
+      create_work_authorization
+      send_email
+    end
+
+    private
+
+      def user
+        return @user if @user
+        @user = User.where(email: @request_attributes[:email]).first_or_initialize do |user| # only hits the do on initialize
+                user.provider = 'auth_link'
+                user.uid = SecureRandom::uuid
+                user.ensure_authentication_token
+        end
+        @user.save
+        @user
       end
-      if @user.save
-        if work_obj != nil && @user.work_authorizations.create(work_title: work_obj.titleValue)
-          work_obj.read_users = [@user.user_key]
+
+      def work_obj
+        return @work_obj if @work_obj
+        @work_obj = DamsObject.where(pid: @work_pid).first if @work_pid
+      end
+
+      def create_work_authorization
+        if user.work_authorizations.create(work_title: work_obj.titleValue)
+          work_obj.read_users = [user.user_key]
           work_obj.save
-          puts 'work authorized'
-        else
-          puts 'work authorization failed'
         end
       end
-      if @user && @user.valid? && work_obj != nil
-        AuthMailer.send_link(@user).deliver_later
-        puts 'email sent!'
-        puts '>>>>>> Task succeeded!'
-      else
-        puts '>>>>>> TASK FAILED'
+
+      def send_email
+        AuthMailer.send_link(user).deliver_later
       end
-    end
   end
 end
