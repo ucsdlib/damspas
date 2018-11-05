1289dbe2cd33d7f01f800e0b55bc821087dd3d75
Kiah <kiah@notch8.com>
2018-11-05 15:17:34 -0800

    replace rails_helper with spec_helper

--- spec/features/auth_link_spec.rb
+++ spec/features/auth_link_spec.rb
@@ -4,7 +4,7 @@ feature 'Auth link' do
   let!(:user) { create_auth_link_user }
 
   # Requesting auth link
-  scenario 'existing user requests sign in link' do
+  scenario "existing user requests sign in link" do
     visit new_auth_link_path
 
     fill_in 'Email', :with => 'test@example.com'
@@ -13,7 +13,7 @@ feature 'Auth link' do
     expect(page).to have_text("Email sent successfully! Check your inbox for the link")
   end
 
-  scenario 'non-existing user requests sign in link' do
+  scenario "non-existing user requests sign in link" do
     visit new_auth_link_path
 
     fill_in 'Email', :with => 'fake@example.com'
@@ -22,7 +22,7 @@ feature 'Auth link' do
     expect(page).to have_text("User doesn't exist")
   end
 
-  scenario 'requesting a sign in link w/ invalid email format' do
+  scenario "requesting a sign in link w/ invalid email format" do
     visit new_auth_link_path
 
     fill_in 'Email', :with => ''
@@ -32,26 +32,26 @@ feature 'Auth link' do
   end
 
   # Sign in via auth link
-  scenario 'user can sign in with a valid email and auth token' do
+  scenario "user can sign in with a valid email and auth token" do
     visit new_user_session_path(email: user.email, auth_token: user.authentication_token)
 
     expect(page).to have_text("Successfully authenticated from email account")
   end
 
-  scenario 'user cannot sign in without a valid email and auth token' do
+  scenario "user cannot sign in without a valid email and auth token" do
     visit new_user_session_path(email: 'fake@example.com', auth_token: 'hacker')
 
     expect(page).to have_text("Authentication failed: Your credentials were invalid")
   end
 
-  scenario 'user can sign out after signing in with a auth token' do
+  scenario "user can sign out after signing in with a auth token" do
     visit new_user_session_path(email: user.email, auth_token: user.authentication_token)
     visit destroy_user_session_path
 
     expect(page).to have_text("You have been logged out of Digital Collections")
   end
 
-  scenario 'user who is already signed in via auth token cannot sign in via another method' do
+  scenario "user who is already signed in via auth token cannot sign in via another method" do
     visit new_user_session_path(email: user.email, auth_token: user.authentication_token)
     visit new_user_session_path
 
@@ -63,7 +63,7 @@ feature 'Auth link' do
   end
 
   # Emails user with auth token
-  scenario 'sends an email if credentials are valid' do
+  scenario "sends an email if credentials are valid" do
     event = AuthMailer.send_link(user)
 
     visit new_user_session_path(email: user.email, auth_token: user.authentication_token)
@@ -72,7 +72,7 @@ feature 'Auth link' do
   end
 
   # What users can view
-  scenario 'user who has authorized works can see the relevant nav tab' do
+  scenario "user who has authorized works can see the relevant nav tab" do
     create_test_dams_object
     user.work_authorizations.create(work_title: 'test_title', work_pid: 'test_pid')
 
@@ -81,20 +81,20 @@ feature 'Auth link' do
     expect(page).to have_text("Authorized Works")
   end
 
-  scenario 'user who does not have authorized works cannot see the relevant nav tab' do
+  scenario "user who does not have authorized works cannot see the relevant nav tab" do
     visit new_user_session_path(email: user.email, auth_token: user.authentication_token)
 
     expect(page).to_not have_text("Authorized Works")
   end
 
   # WorkAuthorizationsController#index
-  scenario 'user without authorized work tries to view work_authorizations_path' do
+  scenario "user without authorized work tries to view work_authorizations_path" do
     visit new_user_session_path(email: user.email, auth_token: user.authentication_token)
 
     expect(page).to have_text("Library Collections").and have_text("Research Data Collections")
   end
 
-  scenario 'user with authorized work tries to view work_authorizations_path' do
+  scenario "user with authorized work tries to view work_authorizations_path" do
     create_test_dams_object
     user.work_authorizations.create(work_title: 'test_title', work_pid: 'test_pid')
 
