8513c5c0cad48a1b7c6d33e29845ae6781f0ecfc
Kiah <kiah@notch8.com>
2018-11-06 16:14:47 -0800

    persist aeon transaction number in the work_auth model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181101205810) do
+ActiveRecord::Schema.define(version: 20181106015745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20181101205810) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "work_pid"
+    t.integer  "aeon_id"
   end
 
 end
