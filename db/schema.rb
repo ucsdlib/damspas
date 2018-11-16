410733269013cb4a62cdfb64b7dd5a3b18df07b5
Benjamin Kiah Stroud <kiah@notch8.com>
2018-11-16 19:07:01 +0000

    Work auth counter cache

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181106015745) do
+ActiveRecord::Schema.define(version: 20181101205810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
