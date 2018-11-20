e452d9afa39858d8bd963bfe5cda787fd0f90dfd
MATTHEW CLARK <matt@notch8.com>
2018-11-20 11:07:45 -0800

    removes unused files for managing aeon queues

--- app/controllers/aeon/queues_controller.rb
+++ app/controllers/aeon/queues_controller.rb
@@ -12,50 +12,10 @@ class Aeon::QueuesController < ApplicationController
   def show
   end
 
-  # GET /aeon/queues/new
-  def new
-    @aeon_queue = Aeon::Queue.new
-  end
-
-  # GET /aeon/queues/1/edit
-  def edit
-  end
-
-  # POST /aeon/queues
-  def create
-    @aeon_queue = Aeon::Queue.new(aeon_queue_params)
-
-    if @aeon_queue.save
-      redirect_to @aeon_queue, notice: 'Queue was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /aeon/queues/1
-  def update
-    if @aeon_queue.update(aeon_queue_params)
-      redirect_to @aeon_queue, notice: 'Queue was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /aeon/queues/1
-  def destroy
-    @aeon_queue.destroy
-    redirect_to aeon_queues_url, notice: 'Queue was successfully destroyed.'
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_aeon_queue
       @aeon_queue = Aeon::Queue.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def aeon_queue_params
-      params[:aeon_queue]
     end
 
     def authorize_user
