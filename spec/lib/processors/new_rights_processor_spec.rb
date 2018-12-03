c54a0c9da82a6331853d6fc38a478c66ee2981d5
MATTHEW CLARK <matt@notch8.com>
2018-12-03 12:42:39 -0800

    refactors specs

--- spec/lib/processors/new_rights_processor_spec.rb
+++ spec/lib/processors/new_rights_processor_spec.rb
@@ -9,6 +9,11 @@ describe Processors::NewRightsProcessor do
   let(:bad_pid){ {subLocation: 'bad_pid'} }
   let(:nil_pid){ {subLocation: nil} }
 
+  before do
+    allow_any_instance_of(Aeon::Request).to receive(:set_to_active).and_return(true)
+    allow_any_instance_of(Aeon::Request).to receive(:set_to_processing).and_return(true)
+  end
+
   describe "initialize" do
     context "if rake task is run with valid credentials" do
       let!(:response){ good_email.merge(good_pid) }
@@ -29,8 +34,8 @@ describe Processors::NewRightsProcessor do
 
   describe "authorize" do
     let!(:dams_object){ DamsObject.create(pid: 'test_pid', titleValue: 'test_title') }
-
     context "when credentials are valid" do
+      let!(:user){ create_auth_link_user } # creates user w/ same email as response 
       let!(:response){ good_email.merge(good_pid) }
       let!(:processor){ Processors::NewRightsProcessor.new(response) }
       it "runs successfully" do
@@ -38,35 +43,27 @@ describe Processors::NewRightsProcessor do
       end
 
       it "sets user attributes correctly" do
-        allow(processor).to receive(:authorize).and_return(processor.send(:user))
-        # @user is not set in processor object until authorize method is run
-        user = processor.instance_variable_get(:@user)
-
+        processor.authorize
+        user.reload
         expect(user.provider).to eq('auth_link')
         expect(user.uid).to_not be_empty
         expect(user.authentication_token).to_not be_empty
       end
 
       it "does not alter the attributes of an existing user" do
-        before_rake_user = create_auth_link_user # creates user w/ same email as response
-        allow(processor).to receive(:authorize).and_return(processor.send(:user))
-        after_rake_user = processor.instance_variable_get(:@user)
-
-        expect(after_rake_user.id).to eq(before_rake_user.id)
-        expect(after_rake_user.email).to eq(before_rake_user.email)
-        expect(after_rake_user.uid).to eq(before_rake_user.uid)
-        expect(after_rake_user.provider).to eq(before_rake_user.provider)
+        user.reload
+        expect{processor.authorize}.to_not change{user.id}
+        expect{processor.authorize}.to_not change{user.email}
+        expect{processor.authorize}.to_not change{user.uid}
+        expect{processor.authorize}.to_not change{user.provider}
       end
 
       it "authorizes the work for the user" do
-        allow(processor).to receive(:authorize).and_return(processor.send(:create_work_authorization))
-        user = processor.instance_variable_get(:@user)
-
-        expect(user.work_authorizations.length).to eq(1)
+        expect{ processor.authorize }.to change{ user.reload and user.work_authorizations.length }.from(0).to(1)
       end
 
       it "sends email to user on success" do
-        expect{ (allow(processor).to receive(:authorize).and_return(processor.send(:user)).and_return(processor.send(:send_email))) }.to change{ ActionMailer::Base.deliveries.count }.by(1)
+        expect{ processor.authorize }.to change{ ActionMailer::Base.deliveries.count }.by(1)
       end
     end
 
