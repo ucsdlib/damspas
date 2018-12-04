3b49acdac785b5245553bcf6772a2ae221a17779
Kiah <kiah@notch8.com>
2018-12-04 13:10:30 -0800

    general cleanup, documentation, & rubocop disables

--- app/controllers/aeon/queues_controller.rb
+++ app/controllers/aeon/queues_controller.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
+<<<<<<< HEAD
 module Aeon
   class QueuesController < ApplicationController
     before_action :authenticate_user!
     before_action :authorize_user
     before_action :set_aeon_queue, only: :show
 
+=======
+
+module Aeon
+  class QueuesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :authorize_user
+    before_action :set_aeon_queue, only: %i[show edit update destroy]
+
+>>>>>>> general cleanup, documentation, & rubocop disables
     # GET /aeon/queues
     def index
       @aeon_queues = Aeon::Queue.all
@@ -17,6 +27,10 @@ module Aeon
       @errors = WorkAuthorization.in_error
     end
 
+<<<<<<< HEAD
+=======
+    # rubocop:disable Layout/IndentationWidth
+>>>>>>> general cleanup, documentation, & rubocop disables
     private
 
       # Use callbacks to share common setup or constraints between actions.
@@ -27,5 +41,9 @@ module Aeon
       def authorize_user
         raise CanCan::AccessDenied unless can? :create, WorkAuthorization
       end
+<<<<<<< HEAD
+=======
+    # rubocop:enable Layout/IndentationWidth
+>>>>>>> general cleanup, documentation, & rubocop disables
   end
 end
