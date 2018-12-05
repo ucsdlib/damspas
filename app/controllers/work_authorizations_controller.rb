42c703c6752139c5d1c914ea632a95c6644b97f4
Kiah <kiah@notch8.com>
2018-12-04 18:17:59 -0800

    addressing minor spacing/indentation fixes throughout the app

--- app/controllers/work_authorizations_controller.rb
+++ app/controllers/work_authorizations_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class WorkAuthorizationsController < ApplicationController
   def index
     @work_authorizations = current_user.work_authorizations.all
-    if @work_authorizations.blank?
+    if @work_authorizations.blank? # rubocop:disable GuardClause, IfUnlessModifier
       redirect_to root_url
     end
   end
