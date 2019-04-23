57f4eda20181e71b6183f7ad32725cd02fd6a43e
Matt Critchlow <mcritchlow@ucsd.edu>
2019-04-23 14:32:59 -0700

    Move Honeycomb init back to config.ru
    It is, apparently, incredibly particular about where it fires up

--- config/initializers/honeycomb.rb
+++
@@ -1,8 +0,0 @@
-# Initializes honeycomb for staging and production
-if %w[staging production].include? Rails.env
-  require 'honeycomb-beeline'
-  Honeycomb.init(
-    writekey: ENV.fetch('APPS_DHH_HONEYCOMB_KEY') { 'default' },
-    dataset: ENV.fetch('APPS_DHH_HONEYCOMB_DATASET') { 'default' }
-  )
-end
