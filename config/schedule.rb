2b2d581ee0d78ea0281acb9d8e76e5a601716a42
Matt Critchlow <mcritchlow@ucsd.edu>
2019-06-10 12:24:03 -0700

    vrr: run cron at different times [prod,stage]

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,21 @@
 env 'RAILS_RELATIVE_URL_ROOT', '/dc'
 
-every 1.hour do
-  rake "aeon_requests:process_new"
-end
-
-every 1.day do
-  rake "aeon_requests:revoke_old"
+# Note: The AEON API currently has a problem processing concurrent requests
+#   So we're offsetting the cron times to try avoiding that for now
+#   This should be able to be deleted in the future
+case @environment
+when 'production'
+  every 1.hour do
+    rake "aeon_requests:process_new"
+  end
+  every 1.day do
+    rake "aeon_requests:revoke_old"
+  end
+when 'staging'
+  every '50 * * * *' do
+    rake "aeon_requests:process_new"
+  end
+  every 1.day, at: '1:00 am' do
+    rake "aeon_requests:revoke_old"
+  end
 end
