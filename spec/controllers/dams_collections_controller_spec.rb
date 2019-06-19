abf633a62fb9d68a1c95de9ff90440ca1c86469c
Matt Critchlow <mcritchlow@ucsd.edu>
2019-06-19 16:01:07 -0700

    specs: fix leaky unit object in controller spec

--- spec/controllers/dams_collections_controller_spec.rb
+++ spec/controllers/dams_collections_controller_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 describe DamsCollectionsController do
   before(:all) do
-    @unit = DamsUnit.create name: "Test Unit", description: "Test Description", code: "tu", uri: "http://example.com/" 
+    @unit = DamsUnit.create name: "Test Unit", description: "Test Description", code: "tu", uri: "http://example.com/"
     @provCollection = DamsProvenanceCollection.create titleValue: "Sample Provenance Collection", visibility: "public"
       @provCollection.save!
-      solr_index (@provCollection.pid) 
+      solr_index (@provCollection.pid)
   end
   after(:all) do
+    @unit.delete
     @provCollection.delete
   end
 
@@ -16,14 +17,14 @@ describe DamsCollectionsController do
       sign_in User.create({:provider => 'developer'})
       get :osf_delete, { id: @provCollection.pid }
       expect(flash[:notice]).to include "Your record has been deleted from OSF Share."
-    end 
+    end
   end
 
   describe "share_config" do
     it "should load config file" do
       @controller = DamsCollectionsController.new
-      @controller.send(:share_config).fetch('token').should == "share_token"  
-      @controller.send(:share_config).fetch('host').should == "https://staging-share.osf.io/"  
+      @controller.send(:share_config).fetch('token').should == "share_token"
+      @controller.send(:share_config).fetch('host').should == "https://staging-share.osf.io/"
     end
-  end 
+  end
 end
