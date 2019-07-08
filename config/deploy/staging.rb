d9fb0c4bfa26dc02753fdefadb827827d9e709c2
Matt Critchlow <mcritchlow@ucsd.edu>
2019-07-08 08:50:32 -0700

    Generate sitemaps on a schedule, not on deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
 set :branch, (ENV['BRANCH'] || fetch(:branch, 'master'))
-server 'lib-hydrahead-staging.ucsd.edu', user: 'conan', roles: %w{web app db sitemap_noping}
+server 'lib-hydrahead-staging.ucsd.edu', user: 'conan', roles: %w{web app db}
 set :rails_env, "staging"
 if ENV["CAP_SSHKEY_STAGING"]
   puts "Using key: #{File.join(ENV["HOME"], ENV["CAP_SSHKEY_STAGING"])}"
