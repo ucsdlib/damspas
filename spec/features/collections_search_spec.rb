f55369d272e6ebe7cf9f6e0c3c2cc63956e2fb99
Matt Critchlow <mcritchlow@ucsd.edu>
2019-07-10 11:12:05 -0700

    Sort RDCP collections by created date

--- spec/features/collections_search_spec.rb
+++ spec/features/collections_search_spec.rb
@@ -9,25 +9,25 @@ feature 'Visitor wants to browse by unit collections' do
       solr_index @unit.pid
       solr_index @oldest_collection.pid
 
-      Timecop.freeze(Date.today + 5) do
-        @newest_collection = DamsProvenanceCollection.create(pid:'collection_2', titleValue: "The New Collection", unitURI: @unit.pid, visibility: "public", resource_type: "text")
-        @newest_collection.damsMetadata.content = File.new('spec/fixtures/damsProvenanceCollection3.rdf.xml').read
-        solr_index @newest_collection.pid
-      end
+      # manipulate ActiveFedora::Base#create_date to create a "newer" collection record
+      allow_any_instance_of(DamsProvenanceCollection).to receive(:create_date).and_return(Time.now + 1.week)
+      @newest_collection = DamsProvenanceCollection.create(pid:'collection_2', titleValue: "The New Collection", unitURI: @unit.pid, visibility: "public", resource_type: "text")
+      @newest_collection.damsMetadata.content = File.new('spec/fixtures/damsProvenanceCollection3.rdf.xml').read
+      solr_index @newest_collection.pid
     end
     after do
       @unit.delete
       @oldest_collection.delete
       @newest_collection.delete
     end
-    xit 'should sort by most recent collection by default, descending to oldest' do
+    it 'should sort by most recent collection by default, descending to oldest' do
       visit dams_unit_collections_path('rdcp')
       old_collection_sort_order = page.body.index('A Old Collection')
       new_collection_sort_order = page.body.index('The New Collection')
       expect(old_collection_sort_order).to be > new_collection_sort_order
     end
 
-    xit 'should still allow user selected sort options' do
+    it 'should still allow user selected sort options' do
       visit dams_unit_collections_path('rdcp', { sort: 'title_ssi asc' })
       old_collection_sort_order = page.body.index('A Old Collection')
       new_collection_sort_order = page.body.index('The New Collection')
