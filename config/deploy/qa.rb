3264d0db5fdc466bf2d24c9e7d885f50e4e6d8c5
Ron Stanonik <rstanonik@ucsd.edu>
2019-07-15 15:40:15 -0700

    Do not deploy solrizer (damspas) to lib-hydratail-qa.

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,7 +1,6 @@
 set :stage, :qa
 set :branch, (ENV['BRANCH'] || fetch(:branch, 'master'))
 server 'lib-hydrahead-qa.ucsd.edu', user: 'conan', roles: %w{web app db}
-server 'lib-hydratail-qa.ucsd.edu', user: 'conan', roles: %w{web app db}
 set :rails_env, "qa"
 if ENV["CAP_SSHKEY_QA"]
   puts "Using key: #{File.join(ENV["HOME"], ENV["CAP_SSHKEY_QA"])}"
